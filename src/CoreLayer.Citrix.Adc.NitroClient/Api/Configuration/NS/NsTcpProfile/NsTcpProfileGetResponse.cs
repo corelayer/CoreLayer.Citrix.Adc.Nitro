8aa27d29c90f56c5420c8612b7ce65b9e8c4e760
Niels Desopper <niels.desopper@corelayer.eu>
2020-04-15 11:44:16 +0200

    Added NsTcpProfileGetResponse.cs + NsTcpProfileConfiguration.cs

---
+++ src/CoreLayer.Citrix.Adc.NitroClient/Api/Configuration/NS/NsTcpProfile/NsTcpProfileGetResponse.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json.Serialization;
+using CoreLayer.Citrix.Adc.NitroData.Api.Configuration.NS;
+
+namespace CoreLayer.Citrix.Adc.NitroClient.Api.Configuration.NS.NsTcpProfile
+{
+    public class NsTcpProfileGetResponse
+    {
+        [JsonPropertyName("nstcpprofile")]
+        public NsTcpProfileConfiguration[] NsTcpProfiles { get; set; }
+    }
+}
