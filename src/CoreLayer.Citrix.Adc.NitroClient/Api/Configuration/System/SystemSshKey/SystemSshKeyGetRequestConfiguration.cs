c1b2b811bf00d9b748881cca588ce096664e5d11
Niels Desopper <niels.desopper@corelayer.eu>
2020-04-21 12:24:15 +0200

    systemsshkey Get command + tests

---
+++ src/CoreLayer.Citrix.Adc.NitroClient/Api/Configuration/System/SystemSshKey/SystemSshKeyGetRequestConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using System.Net.Http;
+using CoreLayer.Citrix.Adc.NitroClient.Interfaces;
+
+namespace CoreLayer.Citrix.Adc.NitroClient.Api.Configuration.System.SystemSshKey
+{
+    public class SystemSshKeyGetRequestConfiguration : NitroRequestConfiguration
+    {
+        public override HttpMethod Method => HttpMethod.Get;
+        public override string ResourcePath => "/nitro/v1/config/systemsshkey";
+        public override INitroRequestDataRoot DataRoot => new SystemSshKeyGetRequestDataRoot();
+        public override INitroRequestOptions Options { get; set; } = new SystemSshKeyGetRequestOptions();
+    }
+}
---
+++ src/CoreLayer.Citrix.Adc.NitroClient/Commands/Configuration/System/SystemSshKey/SystemSshKeyGetCommand.cs
@@ -0,0 +1,14 @@
+﻿using CoreLayer.Citrix.Adc.NitroClient.Api.Configuration.System.SystemSshKey;
+using CoreLayer.Citrix.Adc.NitroClient.Interfaces;
+
+namespace CoreLayer.Citrix.Adc.NitroClient.Commands.Configuration.System.SystemSshKey
+{
+    public class SystemSshKeyGetCommand : NitroCommand<SystemSshKeyGetResponse>
+    {
+        public SystemSshKeyGetCommand(INitroHttpClient client, INitroRequestConfiguration requestConfiguration) : base(
+            client, requestConfiguration)
+        {
+            
+        }
+    }
+}
