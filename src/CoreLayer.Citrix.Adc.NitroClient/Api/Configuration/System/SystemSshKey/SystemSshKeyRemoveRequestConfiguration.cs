f3a32c08ffa4385567e833a45a93598e6ddf1dd3
Niels Desopper <niels.desopper@corelayer.eu>
2020-04-22 09:53:57 +0200

    systemsshkey Remove command + tests

---
+++ src/CoreLayer.Citrix.Adc.NitroClient/Api/Configuration/System/SystemSshKey/SystemSshKeyRemoveRequestConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using System.Net.Http;
+using CoreLayer.Citrix.Adc.NitroClient.Interfaces;
+
+namespace CoreLayer.Citrix.Adc.NitroClient.Api.Configuration.System.SystemSshKey
+{
+    public class SystemSshKeyRemoveRequestConfiguration : NitroRequestConfiguration
+    {
+        public override HttpMethod Method => HttpMethod.Delete;
+        public override string ResourcePath => "/nitro/v1/config/systemsshkey";
+        public override INitroRequestDataRoot DataRoot => new SystemSshKeyRemoveRequestDataRoot();
+        public override INitroRequestOptions Options { get; set; } = new SystemSshKeyRemoveRequestOptions();
+    }
+}
---
+++ src/CoreLayer.Citrix.Adc.NitroClient/Commands/Configuration/System/SystemSshKey/SystemSshKeyRemoveCommand.cs
@@ -0,0 +1,13 @@
+﻿using CoreLayer.Citrix.Adc.NitroClient.Interfaces;
+
+namespace CoreLayer.Citrix.Adc.NitroClient.Commands.Configuration.System.SystemSshKey
+{
+    public class SystemSshKeyRemoveCommand : NitroCommand<NitroResponse>
+    {
+        public SystemSshKeyRemoveCommand(INitroHttpClient client, INitroRequestConfiguration requestConfiguration) :
+            base(client, requestConfiguration)
+        {
+            
+        }
+    }
+}
