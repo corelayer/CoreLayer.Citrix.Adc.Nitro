c9d5419b15cb202b94d2a2a07ca65eb39b29e645
Niels Desopper <niels.desopper@corelayer.eu>
2020-04-23 10:40:11 +0200

    SystemUserAddCommand added + tests

---
+++ src/CoreLayer.Citrix.Adc.NitroClient/Api/Configuration/System/SystemUser/SystemUserAddRequestDataRoot.cs
@@ -0,0 +1,17 @@
+﻿using CoreLayer.Citrix.Adc.NitroClient.Interfaces;
+
+namespace CoreLayer.Citrix.Adc.NitroClient.Api.Configuration.System.SystemUser
+{
+    public class SystemUserAddRequestDataRoot : INitroRequestDataRoot
+    {
+        public SystemUserAddRequestDataRoot(dynamic data)
+        {
+            SystemUser = new dynamic[] {data};
+        }
+        public SystemUserAddRequestDataRoot(dynamic[] data)
+        {
+            SystemUser = data;
+        }
+        public dynamic[] SystemUser { get; set; }
+    }
+}
---
+++ src/CoreLayer.Citrix.Adc.NitroClient/Commands/Configuration/System/SystemUser/SystemUserAddCommand.cs
@@ -0,0 +1,13 @@
+﻿using CoreLayer.Citrix.Adc.NitroClient.Interfaces;
+
+namespace CoreLayer.Citrix.Adc.NitroClient.Commands.Configuration.System.SystemUser
+{
+    public class SystemUserAddCommand : NitroCommand<NitroResponse>
+    {
+        public SystemUserAddCommand(INitroHttpClient client, INitroRequestConfiguration requestConfiguration) : base(
+            client, requestConfiguration)
+        {
+            
+        }
+    }
+}
