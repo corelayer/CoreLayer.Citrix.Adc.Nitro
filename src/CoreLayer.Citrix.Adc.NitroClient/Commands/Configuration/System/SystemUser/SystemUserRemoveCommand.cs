92bba663492f1a1ccafe3919d410abf0793ddacd
Niels Desopper <niels.desopper@corelayer.eu>
2020-04-23 11:09:02 +0200

    SystemUserRemoveCommand added + tests

---
+++ src/CoreLayer.Citrix.Adc.NitroClient/Commands/Configuration/System/SystemUser/SystemUserRemoveCommand.cs
@@ -0,0 +1,13 @@
+﻿using CoreLayer.Citrix.Adc.NitroClient.Interfaces;
+
+namespace CoreLayer.Citrix.Adc.NitroClient.Commands.Configuration.System.SystemUser
+{
+    public class SystemUserRemoveCommand : NitroCommand<NitroResponse>
+    {
+        public SystemUserRemoveCommand(INitroHttpClient client, INitroRequestConfiguration requestConfiguration) : base(
+            client, requestConfiguration)
+        {
+            
+        }
+    }
+}
