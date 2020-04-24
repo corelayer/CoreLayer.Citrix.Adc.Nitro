4530320d7ca3fe86ab31d5ed0184dff7eae69da0
Niels Desopper <niels.desopper@corelayer.eu>
2020-04-24 12:26:26 +0200

    SystemUserSystemCmdPolicyBindingRemoveCommand added + tests

---
+++ src/CoreLayer.Citrix.Adc.NitroClient/Api/Configuration/System/SystemUserSystemCmdPolicyBinding/SystemUserSystemCmdPolicyBindingRemoveRequestConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using System.Net.Http;
+using CoreLayer.Citrix.Adc.NitroClient.Interfaces;
+
+namespace CoreLayer.Citrix.Adc.NitroClient.Api.Configuration.System.SystemUserSystemCmdPolicyBinding
+{
+    public class SystemUserSystemCmdPolicyBindingRemoveRequestConfiguration : NitroRequestConfiguration
+    {
+        public override HttpMethod Method => HttpMethod.Delete;
+        public override string ResourcePath => "/nitro/v1/config/systemuser_systemcmdpolicy_binding";
+        public override INitroRequestDataRoot DataRoot => new SystemUserSystemCmdPolicyBindingRemoveRequestDataRoot();
+        public override INitroRequestOptions Options { get; set; } = new SystemUserSystemCmdPolicyBindingRemoveRequestOptions();
+    }
+}
