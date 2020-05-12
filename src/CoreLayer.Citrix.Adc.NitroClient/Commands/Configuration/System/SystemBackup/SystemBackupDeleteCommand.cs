f27f1d882b1ddf234702a2d894d29bf19963b9a7
Jan Tytgat <jan.tytgat@corelayer.eu>
2020-05-12 15:47:01 +0200

    Update SystemBackupDeleteCommand.cs

--- src/CoreLayer.Citrix.Adc.NitroClient/Commands/Configuration/System/SystemBackup/SystemBackupDeleteCommand.cs
+++ src/CoreLayer.Citrix.Adc.NitroClient/Commands/Configuration/System/SystemBackup/SystemBackupDeleteCommand.cs
@@ -1,3 +1,4 @@
+using CoreLayer.Citrix.Adc.NitroClient.Api.Configuration.System.SystemBackup;
 using CoreLayer.Citrix.Adc.NitroClient.Interfaces;
 
 namespace CoreLayer.Citrix.Adc.NitroClient.Commands.Configuration.System.SystemBackup
