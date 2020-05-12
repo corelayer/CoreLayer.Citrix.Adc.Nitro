e1ca782d85ca3e60623be778dc31b16be5f64ddb
Niels Desopper <niels.desopper@corelayer.eu>
2020-05-12 14:23:39 +0200

    SystemBackupCreateResponse.cs + added to command

--- src/CoreLayer.Citrix.Adc.NitroClient/Commands/Configuration/System/SystemBackup/SystemBackupCreateCommand.cs
+++ src/CoreLayer.Citrix.Adc.NitroClient/Commands/Configuration/System/SystemBackup/SystemBackupCreateCommand.cs
@@ -1,8 +1,9 @@
+using CoreLayer.Citrix.Adc.NitroClient.Api.Configuration.System.SystemBackup;
 using CoreLayer.Citrix.Adc.NitroClient.Interfaces;
 
 namespace CoreLayer.Citrix.Adc.NitroClient.Commands.Configuration.System.SystemBackup
 {
-    public class SystemBackupCreateCommand : NitroCommand<NitroResponse>
+    public class SystemBackupCreateCommand : NitroCommand<SystemBackupCreateResponse>
     {
         public SystemBackupCreateCommand(INitroServiceClient serviceClient, INitroRequestConfiguration requestConfiguration) :
             base(serviceClient, requestConfiguration) { }
