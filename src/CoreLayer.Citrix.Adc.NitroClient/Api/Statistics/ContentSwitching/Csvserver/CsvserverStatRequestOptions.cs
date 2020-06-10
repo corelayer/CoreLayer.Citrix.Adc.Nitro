70447a9c1040908f0ec8b248cbc6e2d17a53e8ea
Niels Desopper <niels.desopper@corelayer.eu>
2020-06-10 11:19:48 +0200

    CsvserverStatCommand + Tests

---
+++ src/CoreLayer.Citrix.Adc.NitroClient/Api/Statistics/ContentSwitching/Csvserver/CsvserverStatRequestOptions.cs
@@ -0,0 +1,7 @@
+﻿namespace CoreLayer.Citrix.Adc.NitroClient.Api.Statistics.ContentSwitching.Csvserver
+{
+    public class CsvserverStatRequestOptions : NitroRequestArgumentOptions
+    {
+        
+    }
+}
---
+++ src/CoreLayer.Citrix.Adc.NitroClientTests/Api/Statistics/ContentSwitching/Csvserver/CsvserverStatRequestConfigurationTest.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using CoreLayer.Citrix.Adc.NitroClient.Api.Statistics.ContentSwitching.Csvserver;
+using Xunit;
+
+namespace CoreLayer.Citrix.Adc.NitroClientTests.Api.Statistics.ContentSwitching.Csvserver
+{
+    public class CsvserverStatRequestConfigurationTest
+    {
+        [Theory]
+        [ClassData(typeof(CsvserverStatRequestConfigurationValidationTestData))]
+        public void CsvserverStatRequestConfigurationValidationTest(CsvserverStatRequestConfiguration configuration, Dictionary<string, string> expected)
+        {
+            Assert.Equal(expected["ResourcePath"], configuration.ResourcePath);
+            Assert.Equal(expected["RequestUri"], configuration.GenerateHttpRequestMessageAsync().Result.RequestUri.ToString());
+            Assert.Equal(expected["Method"], configuration.Method.ToString());
+            Assert.Equal(expected["Method"], configuration.GenerateHttpRequestMessageAsync().Result.Method.ToString());
+            Assert.Equal(expected["Options"], configuration.Options.ToString());
+        }
+    }
+
+    public class CsvserverStatRequestConfigurationValidationTestData : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            yield return new object[]
+            {
+                new CsvserverStatRequestConfiguration(),
+                new Dictionary<string, string>
+                {
+                    {"ResourcePath", "/nitro/v1/stat/csvserver"},
+                    {"Method", "GET"},
+                    {"Options", ""},
+                    {"RequestUri", "/nitro/v1/stat/csvserver"}
+                } 
+            };
+            yield return new object[]
+            {
+                new CsvserverStatRequestConfiguration
+                {
+                    Options = new CsvserverStatRequestOptions
+                    {
+                        ResourceName = "resourceName",
+                        PropertyFilter = new List<string>
+                        {
+                            "wrongFilter"
+                        }
+                    }
+                }, 
+                new Dictionary<string, string>
+                {
+                    {"ResourcePath", "/nitro/v1/stat/csvserver"},
+                    {"Method", "GET"},
+                    {"Options", "/resourceName?attrs=wrongFilter"},
+                    {"RequestUri", "/nitro/v1/stat/csvserver/resourceName?attrs=wrongFilter"}
+                } 
+            };
+            yield return new object[]
+            {
+                new CsvserverStatRequestConfiguration
+                {
+                    Options = new CsvserverStatRequestOptions
+                    {
+                        ResourceName = "resourceName",
+                        PropertyFilter = new List<string>
+                        {
+                            "Name"
+                        }
+                    }
+                }, 
+                new Dictionary<string, string>
+                {
+                    {"ResourcePath", "/nitro/v1/stat/csvserver"},
+                    {"Method", "GET"},
+                    {"Options", "/resourceName?attrs=Name"},
+                    {"RequestUri", "/nitro/v1/stat/csvserver/resourceName?attrs=Name"}
+                } 
+            };
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
