439f4b1abf9cad21a15afedfe47d762e9f5e965c
Niels Desopper <niels.desopper@corelayer.eu>
2020-06-11 11:18:37 +0200

    InterfaceStatCommand + Tests

---
+++ src/CoreLayer.Citrix.Adc.NitroClient/Api/Statistics/Network/Interface/InterfaceStatRequestConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using System.Net.Http;
+using CoreLayer.Citrix.Adc.NitroClient.Interfaces;
+
+namespace CoreLayer.Citrix.Adc.NitroClient.Api.Statistics.Network.Interface
+{
+    public class InterfaceStatRequestConfiguration : NitroRequestConfiguration
+    {
+        public override HttpMethod Method => HttpMethod.Get;
+        public override string ResourcePath => "/nitro/v1/stat/interface";
+        public override INitroRequestOptions Options { get; set; } = new InterfaceStatRequestOptions();
+        public override INitroRequestDataRoot DataRoot => new InterfaceStatRequestDataRoot();
+        
+        public InterfaceStatRequestConfiguration() {}
+    }
+}
