a8bd424fe2d8ef43df20e99b48d1a0c2eac8cbcc
Niels Desopper <niels.desopper@corelayer.eu>
2020-06-11 13:11:01 +0200

    NsStatCommand + Tests

--- src/CoreLayer.Citrix.Adc.NitroClient/Api/Statistics/Ns/Ns/NsStatResponse.cs
+++ src/CoreLayer.Citrix.Adc.NitroClient/Api/Statistics/Ns/Ns/NsStatResponse.cs
@@ -1,7 +1,11 @@
-﻿namespace CoreLayer.Citrix.Adc.NitroClient.Api.Statistics.Ns.Ns
+﻿using System.Text.Json.Serialization;
+using CoreLayer.Citrix.Adc.NitroData.Api.Statistics.Ns;
+
+namespace CoreLayer.Citrix.Adc.NitroClient.Api.Statistics.Ns.Ns
 {
     public class NsStatResponse : NitroResponse
     {
-        
+        [JsonPropertyName("ns")]
+        public NsStats[] NsStatistics { get; set; }
     }
 }
