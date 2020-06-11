830cc04d1d7a8e55d26d5709357c75bf4c92d7a5
Niels Desopper <niels.desopper@corelayer.eu>
2020-06-11 13:36:54 +0200

    ResponderPolicyStatCommand + Tests

---
+++ src/CoreLayer.Citrix.Adc.NitroData/Api/Statistics/Responder/ResponderPolicyStats.cs
@@ -0,0 +1,11 @@
+﻿namespace CoreLayer.Citrix.Adc.NitroData.Api.Statistics.Responder
+{
+    public class ResponderPolicyStats
+    {
+        public string Name { get; set; }
+        public string PiPolicyUndefHits { get; set; }
+        public double? PiPolicyUndefHitsRate { get; set; }
+        public string PiPolicyHits { get; set; }
+        public double? PiPolicyHitsRate { get; set; }
+    }
+}
---
+++ src/CoreLayer.Citrix.Adc.NitroDataTests/Api/Statistics/Responder/ResponderPolicy/ResponderPolicyStatsTest.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text.Json;
+using CoreLayer.Citrix.Adc.NitroClient;
+using CoreLayer.Citrix.Adc.NitroData.Api.Statistics.Responder;
+using Xunit;
+
+namespace CoreLayer.Citrix.Adc.NitroDataTests.Api.Statistics.Responder.ResponderPolicy
+{
+    public class ResponderPolicyStatsTest
+    {
+        [Theory]
+        [ClassData(typeof(ResponderPolicyTestData))]
+        public void ResponderPolicyTests(ResponderPolicyStats responderPolicyStats, Dictionary<string, string> expected)
+        {
+            Assert.Equal(expected["ContentString"], JsonSerializer.Serialize(responderPolicyStats, typeof(ResponderPolicyStats), NitroServiceSerializerOptions.JsonSerializerOptions));
+        }
+    }
+
+    public class ResponderPolicyTestData : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            yield return new object[]
+            {
+                new ResponderPolicyStats()
+                {
+                    
+                }, 
+                new Dictionary<string, string>
+                {
+                    {"ContentString", "{}"}
+                }
+            };
+            yield return new object[]
+            {
+                new ResponderPolicyStats()
+                {
+                    Name = "test"
+                }, 
+                new Dictionary<string, string>
+                {
+                    {"ContentString", "{\"name\":\"test\"}"}
+                }
+            };
+            yield return new object[]
+            {
+                new ResponderPolicyStats()
+                {
+                    Name = "Test",
+                    PiPolicyHitsRate = 5
+                }, 
+                new Dictionary<string, string>
+                {
+                    {"ContentString", "{\"name\":\"Test\",\"pipolicyhitsrate\":5}"}
+                }
+            };
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
