94bfe63bd6778fba33b60bf750bbe00ec395da21
Niels Desopper <niels.desopper@corelayer.eu>
2020-06-12 09:48:32 +0200

    ResponderPolicyLabelStatCommand + tests

---
+++ src/CoreLayer.Citrix.Adc.NitroClient/Api/Statistics/Responder/ResponderPolicyLabel/ResponderPolicyLabelStatRequestOptions.cs
@@ -0,0 +1,7 @@
+﻿namespace CoreLayer.Citrix.Adc.NitroClient.Api.Statistics.Responder.ResponderPolicyLabel
+{
+    public class ResponderPolicyLabelStatRequestOptions : NitroRequestArgumentOptions
+    {
+        
+    }
+}
---
+++ src/CoreLayer.Citrix.Adc.NitroClient/Commands/Statistics/Responder/ResponderPolicyLabel/ResponderPolicyLabelStatCommand.cs
@@ -0,0 +1,11 @@
+﻿using CoreLayer.Citrix.Adc.NitroClient.Api.Statistics.Responder.ResponderPolicyLabel;
+using CoreLayer.Citrix.Adc.NitroClient.Interfaces;
+
+namespace CoreLayer.Citrix.Adc.NitroClient.Commands.Statistics.Responder.ResponderPolicyLabel
+{
+    public class ResponderPolicyLabelStatCommand : NitroCommand<ResponderPolicyLabelStatResponse>
+    {
+        public ResponderPolicyLabelStatCommand(INitroServiceClient serviceClient, INitroRequestConfiguration requestConfiguration) : 
+            base(serviceClient, requestConfiguration) { }
+    }
+}
