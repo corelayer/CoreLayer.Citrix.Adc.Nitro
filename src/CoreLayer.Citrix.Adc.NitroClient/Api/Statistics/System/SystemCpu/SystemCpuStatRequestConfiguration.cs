59875c8e9a108a5316ec17ec1393a2b29635b1e4
Niels Desopper <niels.desopper@corelayer.eu>
2020-06-15 12:15:24 +0200

    SystemCpuStatCommand + tests

---
+++ src/CoreLayer.Citrix.Adc.NitroClient/Api/Statistics/System/SystemCpu/SystemCpuStatRequestConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using System.Net.Http;
+using CoreLayer.Citrix.Adc.NitroClient.Interfaces;
+
+namespace CoreLayer.Citrix.Adc.NitroClient.Api.Statistics.System.SystemCpu
+{
+    public class SystemCpuStatRequestConfiguration : NitroRequestConfiguration
+    {
+        public override HttpMethod Method => HttpMethod.Get;
+        public override string ResourcePath => "/nitro/v1/stat/systemcpu";
+        public override INitroRequestOptions Options { get; set; } = new SystemCpuStatRequestOptions();
+        public override INitroRequestDataRoot DataRoot => new SystemCpuStatRequestDataRoot();
+        
+        public SystemCpuStatRequestConfiguration() {}
+    }
+}
---
+++ src/CoreLayer.Citrix.Adc.NitroDataTests/Api/Statistics/System/SystemCpu/SystemCpuStatsTest.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text.Json;
+using CoreLayer.Citrix.Adc.NitroClient;
+using CoreLayer.Citrix.Adc.NitroData.Api.Statistics.System;
+using Xunit;
+
+namespace CoreLayer.Citrix.Adc.NitroDataTests.Api.Statistics.System.SystemCpu
+{
+    public class SystemCpuStatsTest
+    {
+        [Theory]
+        [ClassData(typeof(SystemCpuStatsTestData))]
+        public void SystemCpuStatsTests(SystemCpuStats systemCpuStats, Dictionary<string, string> expected)
+        {
+            Assert.Equal(expected["ContentString"], JsonSerializer.Serialize(systemCpuStats, typeof(SystemCpuStats), NitroServiceSerializerOptions.JsonSerializerOptions));
+        }
+    }
+
+    public class SystemCpuStatsTestData : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            yield return new object[]
+            {
+                new SystemCpuStats()
+                {
+                    
+                }, 
+                new Dictionary<string, string>
+                {
+                    {"ContentString", "{}"}
+                }
+            };
+            yield return new object[]
+            {
+                new SystemCpuStats()
+                {
+                    Id = "test"
+                }, 
+                new Dictionary<string, string>
+                {
+                    {"ContentString", "{\"id\":\"test\"}"}
+                }
+            };
+            yield return new object[]
+            {
+                new SystemCpuStats()
+                {
+                    Id = "Test",
+                    PerCpuUse = "5"
+                }, 
+                new Dictionary<string, string>
+                {
+                    {"ContentString", "{\"id\":\"Test\",\"percpuuse\":5}"}
+                }
+            };
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
