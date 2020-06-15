a70c8325542b111c22bf77d75fdfc6701760043d
Niels Desopper <niels.desopper@corelayer.eu>
2020-06-15 12:23:28 +0200

    SystemCpuStatCommand + tests

--- src/CoreLayer.Citrix.Adc.NitroClient/Api/Statistics/System/SystemCpu/SystemCpuStatResponse.cs
+++ src/CoreLayer.Citrix.Adc.NitroClient/Api/Statistics/System/SystemCpu/SystemCpuStatResponse.cs
@@ -1,7 +1,11 @@
-﻿namespace CoreLayer.Citrix.Adc.NitroClient.Api.Statistics.System.SystemCpu
+﻿using System.Text.Json.Serialization;
+using CoreLayer.Citrix.Adc.NitroData.Api.Statistics.System;
+
+namespace CoreLayer.Citrix.Adc.NitroClient.Api.Statistics.System.SystemCpu
 {
     public class SystemCpuStatResponse : NitroResponse
     {
-        
+        [JsonPropertyName("systemcpu")]
+        public SystemCpuStats[] SystemCpuStatistics { get; set; }
     }
 }
