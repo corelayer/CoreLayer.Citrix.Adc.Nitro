a7af5292088a6b70a538308c4768bf2d81c9fd92
Niels Desopper <niels.desopper@corelayer.eu>
2020-06-15 10:02:09 +0200

    SslvServerStatCommand.cs + tests

--- src/CoreLayer.Citrix.Adc.NitroClient/Api/Statistics/Ssl/SslvServer/SslvServerStatResponse.cs
+++ src/CoreLayer.Citrix.Adc.NitroClient/Api/Statistics/Ssl/SslvServer/SslvServerStatResponse.cs
@@ -1,7 +1,11 @@
-﻿namespace CoreLayer.Citrix.Adc.NitroClient.Api.Statistics.Ssl.SslvServer
+﻿using System.Text.Json.Serialization;
+using CoreLayer.Citrix.Adc.NitroData.Api.Statistics.Ssl;
+
+namespace CoreLayer.Citrix.Adc.NitroClient.Api.Statistics.Ssl.SslvServer
 {
     public class SslvServerStatResponse : NitroResponse
     {
-        
+        [JsonPropertyName("sslvserver")]
+        public SslvServerStats[] SslvServerStatistics { get; set; }
     }
 }
