cab890cebddfba9c7c431c7a4beb6299f5bd6a48
Niels Desopper <niels.desopper@corelayer.eu>
2020-06-26 09:47:05 +0200

    DnsAaaaRec Add and Remove commands + tests

---
+++ src/CoreLayer.Citrix.Adc.NitroClient/Api/Configuration/Dns/DnsAaaaRec/DnsAaaaRecRemoveRequestConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using System.Net.Http;
+using CoreLayer.Citrix.Adc.NitroClient.Interfaces;
+
+namespace CoreLayer.Citrix.Adc.NitroClient.Api.Configuration.Dns.DnsAaaaRec
+{
+    public class DnsAaaaRecRemoveRequestConfiguration : NitroRequestConfiguration
+    {
+        public override HttpMethod Method => HttpMethod.Delete;
+        public override string ResourcePath => "/nitro/v1/config/dnsaaaarec";
+        public override INitroRequestOptions Options { get; set; } = new DnsAaaaRecRemoveRequestOptions();
+        public override INitroRequestDataRoot DataRoot  => new DnsAaaaRecRemoveRequestDataRoot(); 
+    }
+}
