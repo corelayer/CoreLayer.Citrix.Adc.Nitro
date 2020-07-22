62a745ee22d69d01b9b3f3627a0753b943c0e619
Niels Desopper <niels.desopper@corelayer.eu>
2020-07-22 10:10:46 +0200

    AuthenticationPolicyLabelStatCommand + tests

---
+++ src/CoreLayer.Citrix.Adc.NitroData/Api/Statistics/Authentication/AuthenticationPolicyLabelStats.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json.Serialization;
+
+namespace CoreLayer.Citrix.Adc.NitroData.Api.Statistics.Authentication
+{
+    public class AuthenticationPolicyLabelStats
+    {
+         public string Name { get; set; }
+         [JsonPropertyName("pipolicylabelhits")]public string PolicyLabelHits { get; set; }
+         [JsonPropertyName("pipolicylabelhitsrate")] public double? PolicyLabelHitsRate { get; set; }
+    }
+}
---
+++ src/CoreLayer.Citrix.Adc.NitroDataTests/Api/Statistics/Authentication/AuthenticationPolicyLabel/AuthenticationPolicyLabelStatsTest.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text.Json;
+using CoreLayer.Citrix.Adc.NitroClient;
+using CoreLayer.Citrix.Adc.NitroData.Api.Statistics.Authentication;
+using Xunit;
+
+namespace CoreLayer.Citrix.Adc.NitroDataTests.Api.Statistics.Authentication.AuthenticationPolicyLabel
+{
+    public class AuthenticationPolicyLabelStatsTest
+    {
+        [Theory]
+        [ClassData(typeof(AuthenticationPolicyLabelStatsValidationTestData))]
+        public void AuthenticationPolicyLabelStatsValidationTest(AuthenticationPolicyLabelStats stats,
+            Dictionary<string, string> expected)
+        {
+            Assert.Equal(expected["ContentString"], JsonSerializer.Serialize(stats, typeof(AuthenticationPolicyLabelStats), NitroServiceSerializerOptions.JsonSerializerOptions));
+        }
+    }
+
+    public class AuthenticationPolicyLabelStatsValidationTestData : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            yield return new object[]
+            {
+                new AuthenticationPolicyLabelStats()
+                {
+                    
+                }, 
+                new Dictionary<string, string>
+                {
+                    {"ContentString", "{}"}
+                }
+            };
+            yield return new object[]
+            {
+                new AuthenticationPolicyLabelStats()
+                {
+                    PolicyLabelHits = "test"
+                }, 
+                new Dictionary<string, string>
+                {
+                    {"ContentString", "{\"pipolicylabelhits\":\"test\"}"}
+                }
+            };
+            yield return new object[]
+            {
+                new AuthenticationPolicyLabelStats()
+                {
+                    PolicyLabelHits = "Test",
+                    PolicyLabelHitsRate = 5
+                }, 
+                new Dictionary<string, string>
+                {
+                    {"ContentString", "{\"pipolicylabelhits\":\"Test\",\"pipolicylabelhitsrate\":5}"}
+                }
+            };
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
