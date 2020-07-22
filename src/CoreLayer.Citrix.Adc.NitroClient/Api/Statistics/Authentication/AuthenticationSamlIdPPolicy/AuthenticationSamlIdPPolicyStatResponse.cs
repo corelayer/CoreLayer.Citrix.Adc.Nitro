3b60d42d2dc40327b430a6ceb75cfd5902a4f8f0
Niels Desopper <niels.desopper@corelayer.eu>
2020-07-22 11:01:48 +0200

    AuthenticationSamlIdPPolicyStatCommand + tests

---
+++ src/CoreLayer.Citrix.Adc.NitroClient/Api/Statistics/Authentication/AuthenticationSamlIdPPolicy/AuthenticationSamlIdPPolicyStatResponse.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json.Serialization;
+using CoreLayer.Citrix.Adc.NitroData.Api.Statistics.Authentication;
+
+namespace CoreLayer.Citrix.Adc.NitroClient.Api.Statistics.Authentication.AuthenticationSamlIdPPolicy
+{
+    public class AuthenticationSamlIdPPolicyStatResponse : NitroResponse
+    {
+        [JsonPropertyName("authenticationsamlidppolicy")]public AuthenticationSamlIdPPolicyStats[] AuthenticationSamlIdPPolicyStatistics
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ src/CoreLayer.Citrix.Adc.NitroData/Api/Statistics/Authentication/AuthenticationSamlIdPPolicyStats.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json.Serialization;
+
+namespace CoreLayer.Citrix.Adc.NitroData.Api.Statistics.Authentication
+{
+    public class AuthenticationSamlIdPPolicyStats
+    {
+        public string Name { get; set; }
+        [JsonPropertyName("pipolicyhits")]public string PolicyHits { get; set; }
+        [JsonPropertyName("pipolicyhitsrate")]public double? PolicyHitsRate { get; set; }
+    }
+}
