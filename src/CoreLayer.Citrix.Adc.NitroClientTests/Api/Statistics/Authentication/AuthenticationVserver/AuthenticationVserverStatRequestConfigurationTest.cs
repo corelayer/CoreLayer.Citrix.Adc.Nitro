4d35ce6ab3db78a7a03154653ab30e43d666173d
Niels Desopper <niels.desopper@corelayer.eu>
2020-07-22 11:17:56 +0200

    AuthenticationVserverStatCommand + tests

---
+++ src/CoreLayer.Citrix.Adc.NitroClientTests/Api/Statistics/Authentication/AuthenticationVserver/AuthenticationVserverStatRequestConfigurationTest.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using CoreLayer.Citrix.Adc.NitroClient.Api.Statistics.Authentication.AuthenticationVserver;
+using Xunit;
+
+
+namespace CoreLayer.Citrix.Adc.NitroClientTests.Api.Statistics.Authentication.AuthenticationVserver
+{
+    public class AuthenticationVserverStatRequestConfigurationTest
+    {
+        [Theory]
+        [ClassData(typeof(AuthenticationVserverStatRequestConfigurationValidationTestData))]
+        public void AuthenticationVserverStatRequestConfigurationValidationTest(
+            AuthenticationVserverStatRequestConfiguration configuration, Dictionary<string, string> expected)
+        {
+            Assert.Equal(expected["ResourcePath"], configuration.ResourcePath);
+            Assert.Equal(expected["RequestUri"], configuration.GenerateHttpRequestMessageAsync().Result.RequestUri.ToString());
+            Assert.Equal(expected["Method"], configuration.Method.ToString());
+            Assert.Equal(expected["Method"], configuration.GenerateHttpRequestMessageAsync().Result.Method.ToString());
+            Assert.Equal(expected["Options"], configuration.Options.ToString());
+        }
+    }
+
+    public class AuthenticationVserverStatRequestConfigurationValidationTestData : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            yield return new object[]
+            {
+                new AuthenticationVserverStatRequestConfiguration(),
+                new Dictionary<string, string>
+                {
+                    {"ResourcePath", "/nitro/v1/stat/authenticationvserver"},
+                    {"Method", "GET"},
+                    {"Options", ""},
+                    {"RequestUri", "/nitro/v1/stat/authenticationvserver"}
+                } 
+            };
+            yield return new object[]
+            {
+                new AuthenticationVserverStatRequestConfiguration
+                {
+                    Options = new AuthenticationVserverStatRequestOptions
+                    {
+                        ResourceName = "resourceName",
+                        PropertyFilter = new List<string>
+                        {
+                            "wrongFilter"
+                        }
+                    }
+                }, 
+                new Dictionary<string, string>
+                {
+                    {"ResourcePath", "/nitro/v1/stat/authenticationvserver"},
+                    {"Method", "GET"},
+                    {"Options", "/resourceName?attrs=wrongFilter"},
+                    {"RequestUri", "/nitro/v1/stat/authenticationvserver/resourceName?attrs=wrongFilter"}
+                } 
+            };
+            yield return new object[]
+            {
+                new AuthenticationVserverStatRequestConfiguration
+                {
+                    Options = new AuthenticationVserverStatRequestOptions
+                    {
+                        ResourceName = "resourceName",
+                        PropertyFilter = new List<string>
+                        {
+                            "Name"
+                        }
+                    }
+                }, 
+                new Dictionary<string, string>
+                {
+                    {"ResourcePath", "/nitro/v1/stat/authenticationvserver"},
+                    {"Method", "GET"},
+                    {"Options", "/resourceName?attrs=Name"},
+                    {"RequestUri", "/nitro/v1/stat/authenticationvserver/resourceName?attrs=Name"}
+                } 
+            };
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
