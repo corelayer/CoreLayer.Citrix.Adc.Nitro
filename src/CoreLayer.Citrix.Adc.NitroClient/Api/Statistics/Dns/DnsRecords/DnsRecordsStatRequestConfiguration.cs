2bd18bea66848f2673f5b46b1c481cdda634631a
Niels Desopper <niels.desopper@corelayer.eu>
2020-07-23 12:33:17 +0200

    DnsRecordsStatCommand + tests

---
+++ src/CoreLayer.Citrix.Adc.NitroClient/Api/Statistics/Dns/DnsRecords/DnsRecordsStatRequestConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using System.Net.Http;
+using CoreLayer.Citrix.Adc.NitroClient.Interfaces;
+
+namespace CoreLayer.Citrix.Adc.NitroClient.Api.Statistics.Dns.DnsRecords
+{
+    public class DnsRecordsStatRequestConfiguration : NitroRequestConfiguration
+    {
+         public override HttpMethod Method => HttpMethod.Get;
+         public override string ResourcePath => "/nitro/v1/stat/dnsrecords";
+         public override INitroRequestOptions Options { get; set; } = new DnsRecordsStatRequestOptions();
+         public override INitroRequestDataRoot DataRoot => new DnsRecordsStatRequestDataRoot();
+                                
+         public DnsRecordsStatRequestConfiguration() {}
+    }
+}
---
+++ src/CoreLayer.Citrix.Adc.NitroClientTests/Api/Statistics/Dns/DnsRecords/DnsRecordsStatRequestConfigurationTest.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using CoreLayer.Citrix.Adc.NitroClient.Api.Statistics.Dns.DnsRecords;
+using Xunit;
+
+
+namespace CoreLayer.Citrix.Adc.NitroClientTests.Api.Statistics.Dns.DnsRecords
+{
+    public class DnsRecordsStatRequestConfigurationTest
+    {
+        [Theory]
+        [ClassData(typeof(DnsRecordsStatRequestConfigurationValidationTestData))]
+        public void DnsRecordsStatRequestConfigurationValidationTest(DnsRecordsStatRequestConfiguration configuration,
+            Dictionary<string, string> expected)
+        {
+            Assert.Equal(expected["ResourcePath"], configuration.ResourcePath);
+            Assert.Equal(expected["RequestUri"], configuration.GenerateHttpRequestMessageAsync().Result.RequestUri.ToString());
+            Assert.Equal(expected["Method"], configuration.Method.ToString());
+            Assert.Equal(expected["Method"], configuration.GenerateHttpRequestMessageAsync().Result.Method.ToString());
+            Assert.Equal(expected["Options"], configuration.Options.ToString());
+        }
+    }
+
+    public class DnsRecordsStatRequestConfigurationValidationTestData : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            yield return new object[]
+            {
+                new DnsRecordsStatRequestConfiguration(),
+                new Dictionary<string, string>
+                {
+                    {"ResourcePath", "/nitro/v1/stat/dnsrecords"},
+                    {"Method", "GET"},
+                    {"Options", ""},
+                    {"RequestUri", "/nitro/v1/stat/dnsrecords"}
+                } 
+            };
+            yield return new object[]
+            {
+                new DnsRecordsStatRequestConfiguration
+                {
+                    Options = new DnsRecordsStatRequestOptions
+                    {
+                        ResourceName = "resourceName",
+                        PropertyFilter = new List<string>
+                        {
+                            "wrongFilter"
+                        }
+                    }
+                }, 
+                new Dictionary<string, string>
+                {
+                    {"ResourcePath", "/nitro/v1/stat/dnsrecords"},
+                    {"Method", "GET"},
+                    {"Options", "/resourceName?attrs=wrongFilter"},
+                    {"RequestUri", "/nitro/v1/stat/dnsrecords/resourceName?attrs=wrongFilter"}
+                } 
+            };
+            yield return new object[]
+            {
+                new DnsRecordsStatRequestConfiguration
+                {
+                    Options = new DnsRecordsStatRequestOptions
+                    {
+                        ResourceName = "resourceName",
+                        PropertyFilter = new List<string>
+                        {
+                            "Name"
+                        }
+                    }
+                }, 
+                new Dictionary<string, string>
+                {
+                    {"ResourcePath", "/nitro/v1/stat/dnsrecords"},
+                    {"Method", "GET"},
+                    {"Options", "/resourceName?attrs=Name"},
+                    {"RequestUri", "/nitro/v1/stat/dnsrecords/resourceName?attrs=Name"}
+                } 
+            };
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
