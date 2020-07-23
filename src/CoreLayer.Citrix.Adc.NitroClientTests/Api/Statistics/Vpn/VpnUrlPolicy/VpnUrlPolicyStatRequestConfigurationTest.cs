02563eea2345829b3fa9e1a7c32f70937274174b
Niels Desopper <niels.desopper@corelayer.eu>
2020-07-23 09:08:22 +0200

    VpnUrlPolicyStatCommand + tests

---
+++ src/CoreLayer.Citrix.Adc.NitroClientTests/Api/Statistics/Vpn/VpnUrlPolicy/VpnUrlPolicyStatRequestConfigurationTest.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using CoreLayer.Citrix.Adc.NitroClient.Api.Statistics.Vpn.VpnUrlPolicy;
+using Xunit;
+
+
+namespace CoreLayer.Citrix.Adc.NitroClientTests.Api.Statistics.Vpn.VpnUrlPolicy
+{
+    public class VpnUrlPolicyStatRequestConfigurationTest
+    {
+        [Theory]
+        [ClassData(typeof(VpnUrlPolicyStatRequestConfigurationValidationTestData))]
+        public void VpnUrlPolicyStatRequestConfigurationValidationTest(VpnUrlPolicyStatRequestConfiguration configuration, Dictionary<string, string> expected)
+        {
+            Assert.Equal(expected["ResourcePath"], configuration.ResourcePath);
+            Assert.Equal(expected["RequestUri"], configuration.GenerateHttpRequestMessageAsync().Result.RequestUri.ToString());
+            Assert.Equal(expected["Method"], configuration.Method.ToString());
+            Assert.Equal(expected["Method"], configuration.GenerateHttpRequestMessageAsync().Result.Method.ToString());
+            Assert.Equal(expected["Options"], configuration.Options.ToString());
+        }
+    }
+
+    public class VpnUrlPolicyStatRequestConfigurationValidationTestData : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            yield return new object[]
+            {
+                new VpnUrlPolicyStatRequestConfiguration(),
+                new Dictionary<string, string>
+                {
+                    {"ResourcePath", "/nitro/v1/stat/vpnurlpolicy"},
+                    {"Method", "GET"},
+                    {"Options", ""},
+                    {"RequestUri", "/nitro/v1/stat/vpnurlpolicy"}
+                } 
+            };
+            yield return new object[]
+            {
+                new VpnUrlPolicyStatRequestConfiguration
+                {
+                    Options = new VpnUrlPolicyStatRequestOptions
+                    {
+                        ResourceName = "resourceName",
+                        PropertyFilter = new List<string>
+                        {
+                            "wrongFilter"
+                        }
+                    }
+                }, 
+                new Dictionary<string, string>
+                {
+                    {"ResourcePath", "/nitro/v1/stat/vpnurlpolicy"},
+                    {"Method", "GET"},
+                    {"Options", "/resourceName?attrs=wrongFilter"},
+                    {"RequestUri", "/nitro/v1/stat/vpnurlpolicy/resourceName?attrs=wrongFilter"}
+                } 
+            };
+            yield return new object[]
+            {
+                new VpnUrlPolicyStatRequestConfiguration
+                {
+                    Options = new VpnUrlPolicyStatRequestOptions
+                    {
+                        ResourceName = "resourceName",
+                        PropertyFilter = new List<string>
+                        {
+                            "Name"
+                        }
+                    }
+                }, 
+                new Dictionary<string, string>
+                {
+                    {"ResourcePath", "/nitro/v1/stat/vpnurlpolicy"},
+                    {"Method", "GET"},
+                    {"Options", "/resourceName?attrs=Name"},
+                    {"RequestUri", "/nitro/v1/stat/vpnurlpolicy/resourceName?attrs=Name"}
+                } 
+            };
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ src/CoreLayer.Citrix.Adc.NitroData/Api/Statistics/Vpn/VpnUrlPolicyStats.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json.Serialization;
+
+namespace CoreLayer.Citrix.Adc.NitroData.Api.Statistics.Vpn
+{
+    public class VpnUrlPolicyStats
+    {
+        public string Name { get; set; }
+        [JsonPropertyName("pipolicyhits")]public string PolicyHits { get; set; }
+        [JsonPropertyName("pipolicyhitsrate")]public double? PolicyHitsRate { get; set; }
+    }
+}
