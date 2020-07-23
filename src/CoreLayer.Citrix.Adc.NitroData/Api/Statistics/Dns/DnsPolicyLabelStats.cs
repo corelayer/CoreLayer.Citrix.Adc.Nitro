1620b43cba067fd8c9b1ccc30a05f981d1c8ed0d
Niels Desopper <niels.desopper@corelayer.eu>
2020-07-23 12:03:58 +0200

    DnsPolicyLabelStatCommand + tests

---
+++ src/CoreLayer.Citrix.Adc.NitroData/Api/Statistics/Dns/DnsPolicyLabelStats.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json.Serialization;
+
+namespace CoreLayer.Citrix.Adc.NitroData.Api.Statistics.Dns
+{
+    public class DnsPolicyLabelStats
+    {
+        public string LabelName { get; set; }
+        [JsonPropertyName("pipolicylabelhits")]public string PolicyLabelHits { get; set; }
+        [JsonPropertyName("pipolicylabelhitsrate")]public double? PolicyLabelHitsRate { get; set; }
+    }
+}
---
+++ src/CoreLayer.Citrix.Adc.NitroDataTests/Api/Statistics/Dns/DnsPolicyLabel/DnsPolicyLabelStatsTest.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text.Json;
+using CoreLayer.Citrix.Adc.NitroClient;
+using CoreLayer.Citrix.Adc.NitroData.Api.Statistics.Dns;
+using Xunit;
+
+
+namespace CoreLayer.Citrix.Adc.NitroDataTests.Api.Statistics.Dns.DnsPolicyLabel
+{
+    public class DnsPolicyLabelStatsTest
+    {
+        [Theory]
+        [ClassData(typeof(DnsPolicyLabelStatsValidationTestData))]
+        public void DnsPolicyLabelStatsValidationTest(DnsPolicyLabelStats stats, Dictionary<string, string> expected)
+        {
+            Assert.Equal(expected["ContentString"], JsonSerializer.Serialize(stats, typeof(DnsPolicyLabelStats), NitroServiceSerializerOptions.JsonSerializerOptions));
+        }
+    }
+
+    public class DnsPolicyLabelStatsValidationTestData : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            yield return new object[]
+            {
+                new DnsPolicyLabelStats()
+                {
+                    
+                }, 
+                new Dictionary<string, string>
+                {
+                    {"ContentString", "{}"}
+                }
+            };
+            yield return new object[]
+            {
+                new DnsPolicyLabelStats()
+                {
+                    LabelName = "test"
+                }, 
+                new Dictionary<string, string>
+                {
+                    {"ContentString", "{\"labelname\":\"test\"}"}
+                }
+            };
+            yield return new object[]
+            {
+                new DnsPolicyLabelStats()
+                {
+                    LabelName = "Test",
+                    PolicyLabelHitsRate = 5
+                }, 
+                new Dictionary<string, string>
+                {
+                    {"ContentString", "{\"labelname\":\"Test\",\"pipolicylabelhitsrate\":5}"}
+                }
+            };
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
