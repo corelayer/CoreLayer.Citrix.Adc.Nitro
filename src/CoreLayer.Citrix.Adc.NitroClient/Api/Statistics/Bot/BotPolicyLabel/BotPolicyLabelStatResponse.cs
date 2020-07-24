2303b4377e76205079a2c6951bc63de25b2722a3
Niels Desopper <niels.desopper@corelayer.eu>
2020-07-24 12:18:16 +0200

    BotPolicyLabelStatCommand + tests

---
+++ src/CoreLayer.Citrix.Adc.NitroClient/Api/Statistics/Bot/BotPolicyLabel/BotPolicyLabelStatResponse.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json.Serialization;
+using CoreLayer.Citrix.Adc.NitroData.Api.Statistics.Bot;
+
+namespace CoreLayer.Citrix.Adc.NitroClient.Api.Statistics.Bot.BotPolicyLabel
+{
+    public class BotPolicyLabelStatResponse : NitroResponse
+    {
+        [JsonPropertyName("botpolicylabel")]public BotPolicyLabelStats[] BotPolicyLabelStatistics { get; set; }
+    }
+}
---
+++ src/CoreLayer.Citrix.Adc.NitroDataTests/Api/Statistics/Bot/BotPolicyLabel/BotPolicyLabelStatsTest.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text.Json;
+using CoreLayer.Citrix.Adc.NitroClient;
+using CoreLayer.Citrix.Adc.NitroData.Api.Statistics.Bot;
+using Xunit;
+
+
+namespace CoreLayer.Citrix.Adc.NitroDataTests.Api.Statistics.Bot.BotPolicyLabel
+{
+    public class BotPolicyLabelStatsTest
+    {
+        [Theory]
+        [ClassData(typeof(BotPolicyLabelStatsValidationTestData))]
+        public void BotPolicyLabelStatsValidationTest(BotPolicyLabelStats stats, Dictionary<string, string> expected)
+        {
+            Assert.Equal(expected["ContentString"], JsonSerializer.Serialize(stats, typeof(BotPolicyLabelStats), NitroServiceSerializerOptions.JsonSerializerOptions));
+        }
+    }
+
+    public class BotPolicyLabelStatsValidationTestData : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            yield return new object[]
+            {
+                new BotPolicyLabelStats()
+                {
+                    
+                }, 
+                new Dictionary<string, string>
+                {
+                    {"ContentString", "{}"}
+                }
+            };
+            yield return new object[]
+            {
+                new BotPolicyLabelStats()
+                {
+                    LabelName = "test"
+                }, 
+                new Dictionary<string, string>
+                {
+                    {"ContentString", "{\"labelname\":\"test\"}"}
+                }
+            };
+            yield return new object[]
+            {
+                new BotPolicyLabelStats()
+                {
+                    LabelName = "Test",
+                    PolicyLabelHitsRate = 5
+                }, 
+                new Dictionary<string, string>
+                {
+                    {"ContentString", "{\"labelname\":\"Test\",\"pipolicylabelhitsrate\":5}"}
+                }
+            };
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
