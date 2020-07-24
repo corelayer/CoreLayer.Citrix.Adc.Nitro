1cf6a453b819a7e3553758873a2b3b18fa459571
Niels Desopper <niels.desopper@corelayer.eu>
2020-07-24 11:27:49 +0200

    BotPolicyStatCommand + tests

---
+++ src/CoreLayer.Citrix.Adc.NitroClient/Api/Statistics/Bot/BotPolicy/BotPolicyStatRequestOptions.cs
@@ -0,0 +1,7 @@
+﻿namespace CoreLayer.Citrix.Adc.NitroClient.Api.Statistics.Bot.BotPolicy
+{
+    public class BotPolicyStatRequestOptions : NitroRequestArgumentOptions
+    {
+        
+    }
+}
---
+++ src/CoreLayer.Citrix.Adc.NitroClient/Commands/Statistics/Bot/BotPolicy/BotPolicyStatCommand.cs
@@ -0,0 +1,15 @@
+﻿using CoreLayer.Citrix.Adc.NitroClient.Api.Statistics.Bot.BotPolicy;
+using CoreLayer.Citrix.Adc.NitroClient.Interfaces;
+
+namespace CoreLayer.Citrix.Adc.NitroClient.Commands.Statistics.Bot.BotPolicy
+{
+    public class BotPolicyStatCommand : NitroCommand<BotPolicyStatResponse>
+    {
+        public BotPolicyStatCommand(INitroServiceClient serviceClient, 
+            INitroRequestConfiguration requestConfiguration) :
+            base(serviceClient, requestConfiguration)
+        {
+            
+        }
+    }
+}
