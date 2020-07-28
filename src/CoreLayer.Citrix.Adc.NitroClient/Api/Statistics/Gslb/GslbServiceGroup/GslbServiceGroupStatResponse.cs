9b0160634ce738d12dd6637d5cc416dcec88270c
Niels Desopper <niels.desopper@corelayer.eu>
2020-07-28 12:38:49 +0200

    GslbServiceStatCommand + test

---
+++ src/CoreLayer.Citrix.Adc.NitroClient/Api/Statistics/Gslb/GslbServiceGroup/GslbServiceGroupStatResponse.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json.Serialization;
+using CoreLayer.Citrix.Adc.NitroData.Api.Statistics.Gslb;
+
+namespace CoreLayer.Citrix.Adc.NitroClient.Api.Statistics.Gslb.GslbServiceGroup
+{
+    public class GslbServiceGroupStatResponse : NitroResponse
+    {
+        [JsonPropertyName("gslbservicegroup")]public GslbServiceGroupStats[] GslbServiceGroupStatistics { get; set; }
+    }
+}
---
+++ src/CoreLayer.Citrix.Adc.NitroDataTests/Api/Statistics/Gslb/GslbServiceGroup/GslbServiceGroupStatsTest.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text.Json;
+using CoreLayer.Citrix.Adc.NitroClient;
+using CoreLayer.Citrix.Adc.NitroData.Api.Statistics.Gslb;
+using Xunit;
+
+namespace CoreLayer.Citrix.Adc.NitroDataTests.Api.Statistics.Gslb.GslbServiceGroup
+{
+    public class GslbServiceGroupStatsTest
+    {
+        [Theory]
+        [ClassData(typeof(GslbServiceGroupStatsValidationTestData))]
+        public void GslbServiceGroupStatsValidationTest(GslbServiceGroupStats stats,
+            Dictionary<string, string> expected)
+        {
+            Assert.Equal(expected["ContentString"], JsonSerializer.Serialize(stats, typeof(GslbServiceGroupStats), NitroServiceSerializerOptions.JsonSerializerOptions));
+        }
+    }
+
+    public class GslbServiceGroupStatsValidationTestData : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            yield return new object[]
+            {
+                new GslbServiceGroupStats()
+                {
+                    
+                }, 
+                new Dictionary<string, string>
+                {
+                    {"ContentString", "{}"}
+                }
+            };
+            yield return new object[]
+            {
+                new GslbServiceGroupStats()
+                {
+                    ServiceGroupName = "test"
+                }, 
+                new Dictionary<string, string>
+                {
+                    {"ContentString", "{\"servicegroupname\":\"test\"}"}
+                }
+            };
+            yield return new object[]
+            {
+                new GslbServiceGroupStats()
+                {
+                    ServiceGroupName = "Test",
+                    State = "5"
+                }, 
+                new Dictionary<string, string>
+                {
+                    {"ContentString", "{\"servicegroupname\":\"Test\",\"state\":\"5\"}"}
+                }
+            };
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
