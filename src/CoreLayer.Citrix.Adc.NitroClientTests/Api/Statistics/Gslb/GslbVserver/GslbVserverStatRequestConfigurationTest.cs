17ea4e37bc5b706ff0b923dc1a623838443ffec1
Niels Desopper <niels.desopper@corelayer.eu>
2020-07-29 11:21:26 +0200

    GslbVserverStatCommand + test

---
+++ src/CoreLayer.Citrix.Adc.NitroClientTests/Api/Statistics/Gslb/GslbVserver/GslbVserverStatRequestConfigurationTest.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using CoreLayer.Citrix.Adc.NitroClient.Api.Statistics.Gslb.GslbVserver;
+using Xunit;
+
+
+namespace CoreLayer.Citrix.Adc.NitroClientTests.Api.Statistics.Gslb.GslbVserver
+{
+    public class GslbVserverStatRequestConfigurationTest
+    {
+        [Theory]
+        [ClassData(typeof(GslbVserverStatRequestConfigurationValidationTestData))]
+        public void GslbVserverStatRequestConfigurationValidationTest(GslbVserverStatRequestConfiguration configuration,
+            Dictionary<string, string> expected)
+        {
+            Assert.Equal(expected["ResourcePath"], configuration.ResourcePath);
+            Assert.Equal(expected["RequestUri"], configuration.GenerateHttpRequestMessageAsync().Result.RequestUri.ToString());
+            Assert.Equal(expected["Method"], configuration.Method.ToString());
+            Assert.Equal(expected["Method"], configuration.GenerateHttpRequestMessageAsync().Result.Method.ToString());
+            Assert.Equal(expected["Options"], configuration.Options.ToString());
+        }
+    }
+
+    public class GslbVserverStatRequestConfigurationValidationTestData : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            yield return new object[]
+            {
+                new GslbVserverStatRequestConfiguration(),
+                new Dictionary<string, string>
+                {
+                    {"ResourcePath", "/nitro/v1/stat/gslbvserver"},
+                    {"Method", "GET"},
+                    {"Options", ""},
+                    {"RequestUri", "/nitro/v1/stat/gslbvserver"}
+                } 
+            };
+            yield return new object[]
+            {
+                new GslbVserverStatRequestConfiguration
+                {
+                    Options = new GslbVserverStatRequestOptions
+                    {
+                        ResourceName = "resourceName",
+                        PropertyFilter = new List<string>
+                        {
+                            "wrongFilter"
+                        }
+                    }
+                }, 
+                new Dictionary<string, string>
+                {
+                    {"ResourcePath", "/nitro/v1/stat/gslbvserver"},
+                    {"Method", "GET"},
+                    {"Options", "/resourceName?attrs=wrongFilter"},
+                    {"RequestUri", "/nitro/v1/stat/gslbvserver/resourceName?attrs=wrongFilter"}
+                } 
+            };
+            yield return new object[]
+            {
+                new GslbVserverStatRequestConfiguration
+                {
+                    Options = new GslbVserverStatRequestOptions
+                    {
+                        ResourceName = "resourceName",
+                        PropertyFilter = new List<string>
+                        {
+                            "Name"
+                        }
+                    }
+                }, 
+                new Dictionary<string, string>
+                {
+                    {"ResourcePath", "/nitro/v1/stat/gslbvserver"},
+                    {"Method", "GET"},
+                    {"Options", "/resourceName?attrs=Name"},
+                    {"RequestUri", "/nitro/v1/stat/gslbvserver/resourceName?attrs=Name"}
+                } 
+            };
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
