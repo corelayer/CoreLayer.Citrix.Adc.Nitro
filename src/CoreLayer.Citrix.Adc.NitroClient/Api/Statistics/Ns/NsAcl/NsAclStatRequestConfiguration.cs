c2dd78f06250c710de60863940833dca49cc086c
Niels Desopper <niels.desopper@corelayer.eu>
2020-08-03 11:17:29 +0200

    NsAclStatCommand + test

---
+++ src/CoreLayer.Citrix.Adc.NitroClient/Api/Statistics/Ns/NsAcl/NsAclStatRequestConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using System.Net.Http;
+using CoreLayer.Citrix.Adc.NitroClient.Interfaces;
+
+namespace CoreLayer.Citrix.Adc.NitroClient.Api.Statistics.Ns.NsAcl
+{
+    public class NsAclStatRequestConfiguration : NitroRequestConfiguration
+    {
+        public override HttpMethod Method => HttpMethod.Get;
+        public override string ResourcePath => "/nitro/v1/stat/nsacl";
+        public override INitroRequestOptions Options { get; set; } = new NsAclStatRequestOptions();
+        public override INitroRequestDataRoot DataRoot => new NsAclStatRequestDataRoot();
+        
+        public NsAclStatRequestConfiguration() {}
+    }
+}
