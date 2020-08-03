c1b93f0a33805d0ff25b9df28ff82f34e6908483
Niels Desopper <niels.desopper@corelayer.eu>
2020-08-03 10:20:48 +0200

    VxlanStatCommand + test

---
+++ src/CoreLayer.Citrix.Adc.NitroClient/Api/Statistics/Network/Vxlan/VxlanStatResponse.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json.Serialization;
+using CoreLayer.Citrix.Adc.NitroData.Api.Statistics.Network;
+
+namespace CoreLayer.Citrix.Adc.NitroClient.Api.Statistics.Network.Vxlan
+{
+    public class VxlanStatResponse : NitroResponse
+    {
+        [JsonPropertyName("vxlan")]public VxlanStats[] VxlanStatistics { get; set; }
+    }
+}
---
+++ src/CoreLayer.Citrix.Adc.NitroClientTests/Api/Statistics/Network/Vxlan/VxlanStatRequestConfigurationTest.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using CoreLayer.Citrix.Adc.NitroClient.Api.Statistics.Network.Vxlan;
+using Xunit;
+
+
+namespace CoreLayer.Citrix.Adc.NitroClientTests.Api.Statistics.Network.Vxlan
+{
+    public class VxlanStatRequestConfigurationTest
+    {
+        [Theory]
+        [ClassData(typeof(VxlanStatRequestConfigurationValidationTestData))]
+        public void VxlanStatRequestConfigurationValidationTest(VxlanStatRequestConfiguration configuration,
+            Dictionary<string, string> expected)
+        {
+            Assert.Equal(expected["ResourcePath"], configuration.ResourcePath);
+            Assert.Equal(expected["RequestUri"], configuration.GenerateHttpRequestMessageAsync().Result.RequestUri.ToString());
+            Assert.Equal(expected["Method"], configuration.Method.ToString());
+            Assert.Equal(expected["Method"], configuration.GenerateHttpRequestMessageAsync().Result.Method.ToString());
+            Assert.Equal(expected["Options"], configuration.Options.ToString());
+        }
+    }
+
+    public class VxlanStatRequestConfigurationValidationTestData : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            yield return new object[]
+            {
+                new VxlanStatRequestConfiguration(),
+                new Dictionary<string, string>
+                {
+                    {"ResourcePath", "/nitro/v1/stat/vxlan"},
+                    {"Method", "GET"},
+                    {"Options", ""},
+                    {"RequestUri", "/nitro/v1/stat/vxlan"}
+                } 
+            };
+            yield return new object[]
+            {
+                new VxlanStatRequestConfiguration
+                {
+                    Options = new VxlanStatRequestOptions
+                    {
+                        ResourceName = "resourceName",
+                        PropertyFilter = new List<string>
+                        {
+                            "wrongFilter"
+                        }
+                    }
+                }, 
+                new Dictionary<string, string>
+                {
+                    {"ResourcePath", "/nitro/v1/stat/vxlan"},
+                    {"Method", "GET"},
+                    {"Options", "/resourceName?attrs=wrongFilter"},
+                    {"RequestUri", "/nitro/v1/stat/vxlan/resourceName?attrs=wrongFilter"}
+                } 
+            };
+            yield return new object[]
+            {
+                new VxlanStatRequestConfiguration
+                {
+                    Options = new VxlanStatRequestOptions
+                    {
+                        ResourceName = "resourceName",
+                        PropertyFilter = new List<string>
+                        {
+                            "Name"
+                        }
+                    }
+                }, 
+                new Dictionary<string, string>
+                {
+                    {"ResourcePath", "/nitro/v1/stat/vxlan"},
+                    {"Method", "GET"},
+                    {"Options", "/resourceName?attrs=Name"},
+                    {"RequestUri", "/nitro/v1/stat/vxlan/resourceName?attrs=Name"}
+                } 
+            };
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
