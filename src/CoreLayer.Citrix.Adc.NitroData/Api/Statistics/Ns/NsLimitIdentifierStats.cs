16d300c57fa1d5a3395dfc6611c004e0743b78dc
Niels Desopper <niels.desopper@corelayer.eu>
2020-08-04 09:50:05 +0200

    NsLimitIdentifierStatCommand + test

---
+++ src/CoreLayer.Citrix.Adc.NitroData/Api/Statistics/Ns/NsLimitIdentifierStats.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace CoreLayer.Citrix.Adc.NitroData.Api.Statistics.Ns
+{
+    public class NsLimitIdentifierStats
+    {
+        public string Name { get; set; }
+        [JsonPropertyName("ratelmtobjhits")]public string RateLimitObjectHits { get; set; }
+        [JsonPropertyName("ratelmtobjdrops")]public string RateLimitObjectDrops { get; set; }
+        [JsonPropertyName("ratelmtsessionobjhits")]public string RateLimitSessionObjectHits { get; set; }
+    }
+}
---
+++ src/CoreLayer.Citrix.Adc.NitroDataTests/Api/Statistics/Ns/NsLimitIdentifier/NsLimitIdentifierStatsTest.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text.Json;
+using CoreLayer.Citrix.Adc.NitroClient;
+using CoreLayer.Citrix.Adc.NitroData.Api.Statistics.Ns;
+using Xunit;
+
+namespace CoreLayer.Citrix.Adc.NitroDataTests.Api.Statistics.Ns.NsLimitIdentifier
+{
+    public class NsLimitIdentifierStatsTest
+    {
+        [Theory]
+        [ClassData(typeof(NsLimitIdentifierStatsValidationTestData))]
+        public void NsLimitIdentifierStatsValidationTest(NsLimitIdentifierStats stats,
+            Dictionary<string, string> expected)
+        {
+            Assert.Equal(expected["ContentString"], JsonSerializer.Serialize(stats, typeof(NsLimitIdentifierStats), NitroServiceSerializerOptions.JsonSerializerOptions));
+        }
+    }
+
+    public class NsLimitIdentifierStatsValidationTestData : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            yield return new object[]
+            {
+                new NsLimitIdentifierStats()
+                {
+                    
+                }, 
+                new Dictionary<string, string>
+                {
+                    {"ContentString", "{}"}
+                }
+            };
+            yield return new object[]
+            {
+                new NsLimitIdentifierStats()
+                {
+                    Name = "test"
+                }, 
+                new Dictionary<string, string>
+                {
+                    {"ContentString", "{\"name\":\"test\"}"}
+                }
+            };
+            yield return new object[]
+            {
+                new NsLimitIdentifierStats()
+                {
+                    Name = "Test",
+                    RateLimitObjectDrops = "5"
+                }, 
+                new Dictionary<string, string>
+                {
+                    {"ContentString", "{\"name\":\"Test\",\"ratelmtobjdrops\":\"5\"}"}
+                }
+            };
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
