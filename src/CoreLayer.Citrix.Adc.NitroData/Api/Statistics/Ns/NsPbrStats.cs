b97bee5612e2bef0fdc69f2378addff45187c5c3
Niels Desopper <niels.desopper@corelayer.eu>
2020-08-05 09:15:23 +0200

    NsPbrStatCommand + test

---
+++ src/CoreLayer.Citrix.Adc.NitroData/Api/Statistics/Ns/NsPbrStats.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json.Serialization;
+
+namespace CoreLayer.Citrix.Adc.NitroData.Api.Statistics.Ns
+{
+    public class NsPbrStats
+    {
+        public string Name { get; set; }
+        [JsonPropertyName("pbrtotpktsallowed")]public string PbrTotalPacketsAllowed { get; set; }
+        [JsonPropertyName("pbrpktsallowedrate")]public double? PbrPacketsAllowedRate { get; set; }
+        [JsonPropertyName("pbrtotpktsdenied")]public string PbrTotalPacketsDenied { get; set; }
+        [JsonPropertyName("pbrpktsdeniedrate")]public double? PbrPacketsDeniedRate { get; set; }
+        [JsonPropertyName("pbrtothits")]public string PbrTotalHits { get; set; }
+        [JsonPropertyName("pbrhitsrate")]public double? PbrTotalHitsRate { get; set; }
+        [JsonPropertyName("pbrtotmisses")]public string PbrTotalMisses { get; set; }
+        [JsonPropertyName("pbrmissesrate")]public double? PbrMissesRate { get; set; }
+        [JsonPropertyName("pbrtotnulldrop")]public string PbrTotalNullDrop { get; set; }
+        [JsonPropertyName("pbrnulldroprate")]public double? PbrNullDropRate { get; set; }
+        [JsonPropertyName("pbrperhits")]public string PbrHits { get; set; }
+        [JsonPropertyName("pbrperhitsrate")]public double? PbrHitsRate { get; set; }
+    }
+}
---
+++ src/CoreLayer.Citrix.Adc.NitroDataTests/Api/Statistics/Ns/NsPbr/NsPbrStatsTest.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text.Json;
+using CoreLayer.Citrix.Adc.NitroClient;
+using CoreLayer.Citrix.Adc.NitroData.Api.Statistics.Ns;
+using Xunit;
+
+namespace CoreLayer.Citrix.Adc.NitroDataTests.Api.Statistics.Ns.NsPbr
+{
+    public class NsPbrStatsTest
+    {
+        [Theory]
+        [ClassData(typeof(NsPbrStatsValidationTestData))]
+        public void NsPbrStatsValidationTest(NsPbrStats stats, Dictionary<string, string> expected)
+        {
+            Assert.Equal(expected["ContentString"], JsonSerializer.Serialize(stats, typeof(NsPbrStats), NitroServiceSerializerOptions.JsonSerializerOptions));
+        }
+    }
+
+    public class NsPbrStatsValidationTestData : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            yield return new object[]
+            {
+                new NsPbrStats()
+                {
+                    
+                }, 
+                new Dictionary<string, string>
+                {
+                    {"ContentString", "{}"}
+                }
+            };
+            yield return new object[]
+            {
+                new NsPbrStats()
+                {
+                    Name = "test"
+                }, 
+                new Dictionary<string, string>
+                {
+                    {"ContentString", "{\"name\":\"test\"}"}
+                }
+            };
+            yield return new object[]
+            {
+                new NsPbrStats()
+                {
+                    Name = "Test",
+                    PbrHitsRate = 5
+                }, 
+                new Dictionary<string, string>
+                {
+                    {"ContentString", "{\"name\":\"Test\",\"pbrperhitsrate\":5}"}
+                }
+            };
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
