7474f8e1a4872b007b57583e46a1ec452aa12488
Niels Desopper <niels.desopper@corelayer.eu>
2020-08-05 09:40:01 +0200

    NsPbr6StatCommand + test

---
+++ src/CoreLayer.Citrix.Adc.NitroClient/Api/Statistics/Ns/NsPbr6/NsPbr6StatResponse.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json.Serialization;
+using CoreLayer.Citrix.Adc.NitroData.Api.Statistics.Ns;
+
+namespace CoreLayer.Citrix.Adc.NitroClient.Api.Statistics.Ns.NsPbr6
+{
+    public class NsPbr6StatResponse : NitroResponse
+    {
+        [JsonPropertyName("nspbr6")]public NsPbr6Stats[] NsPbr6Statistics { get; set; }
+    }
+}
---
+++ src/CoreLayer.Citrix.Adc.NitroClientTests/Api/Statistics/Ns/NsPbr6/NsPbr6StatRequestConfigurationTest.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using CoreLayer.Citrix.Adc.NitroClient.Api.Statistics.Ns.NsPbr6;
+using Xunit;
+
+namespace CoreLayer.Citrix.Adc.NitroClientTests.Api.Statistics.Ns.NsPbr6
+{
+    public class NsPbr6StatRequestConfigurationTest
+    {
+        [Theory]
+        [ClassData(typeof(NsPbr6StatRequestConfigurationValidationTestData))]
+        public void NsPbr6StatRequestConfigurationValidationTest(NsPbr6StatRequestConfiguration configuration,
+            Dictionary<string, string> expected)
+        {
+            Assert.Equal(expected["ResourcePath"], configuration.ResourcePath);
+            Assert.Equal(expected["RequestUri"], configuration.GenerateHttpRequestMessageAsync().Result.RequestUri.ToString());
+            Assert.Equal(expected["Method"], configuration.Method.ToString());
+            Assert.Equal(expected["Method"], configuration.GenerateHttpRequestMessageAsync().Result.Method.ToString());
+            Assert.Equal(expected["Options"], configuration.Options.ToString());
+        }
+    }
+
+    public class NsPbr6StatRequestConfigurationValidationTestData : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            yield return new object[]
+            {
+                new NsPbr6StatRequestConfiguration(),
+                new Dictionary<string, string>
+                {
+                    {"ResourcePath", "/nitro/v1/stat/nspbr6"},
+                    {"Method", "GET"},
+                    {"Options", ""},
+                    {"RequestUri", "/nitro/v1/stat/nspbr6"}
+                } 
+            };
+            yield return new object[]
+            {
+                new NsPbr6StatRequestConfiguration
+                {
+                    Options = new NsPbr6StatRequestOptions
+                    {
+                        ResourceName = "resourceName",
+                        PropertyFilter = new List<string>
+                        {
+                            "wrongFilter"
+                        }
+                    }
+                }, 
+                new Dictionary<string, string>
+                {
+                    {"ResourcePath", "/nitro/v1/stat/nspbr6"},
+                    {"Method", "GET"},
+                    {"Options", "/resourceName?attrs=wrongFilter"},
+                    {"RequestUri", "/nitro/v1/stat/nspbr6/resourceName?attrs=wrongFilter"}
+                } 
+            };
+            yield return new object[]
+            {
+                new NsPbr6StatRequestConfiguration
+                {
+                    Options = new NsPbr6StatRequestOptions
+                    {
+                        ResourceName = "resourceName",
+                        PropertyFilter = new List<string>
+                        {
+                            "Name"
+                        }
+                    }
+                }, 
+                new Dictionary<string, string>
+                {
+                    {"ResourcePath", "/nitro/v1/stat/nspbr6"},
+                    {"Method", "GET"},
+                    {"Options", "/resourceName?attrs=Name"},
+                    {"RequestUri", "/nitro/v1/stat/nspbr6/resourceName?attrs=Name"}
+                } 
+            };
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
