3b6cb171071c87a6a3ecf9b61f804769f3e31f50
Niels Desopper <niels.desopper@corelayer.eu>
2020-08-14 10:34:32 +0200

    ExtendedMemoryParamUnsetCommand + tests

---
+++ src/CoreLayer.Citrix.Adc.NitroClient/Api/Configuration/Basic/ExtendedMemoryParam/ExtendedMemoryParamUnsetRequestConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using System.Net.Http;
+using CoreLayer.Citrix.Adc.NitroClient.Interfaces;
+
+namespace CoreLayer.Citrix.Adc.NitroClient.Api.Configuration.Basic.ExtendedMemoryParam
+{
+    public class ExtendedMemoryParamUnsetRequestConfiguration : NitroRequestConfiguration
+    {
+        public override HttpMethod Method => HttpMethod.Post;
+        public override string ResourcePath => "/nitro/v1/config/extendedmemoryparam";
+        public override INitroRequestDataRoot DataRoot => new ExtendedMemoryParamUnsetRequestDataRoot(); 
+        public override INitroRequestOptions Options  => new ExtendedMemoryParamUnsetRequestOptions(){Action = "unset"};
+                
+        public ExtendedMemoryParamUnsetRequestConfiguration()
+        {
+            
+        }
+    }
+}
---
+++ src/CoreLayer.Citrix.Adc.NitroClient/Api/Configuration/Basic/ExtendedMemoryParam/ExtendedMemoryParamUnsetRequestData.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json.Serialization;
+using CoreLayer.Citrix.Adc.NitroClient.Interfaces;
+
+namespace CoreLayer.Citrix.Adc.NitroClient.Api.Configuration.Basic.ExtendedMemoryParam
+{
+    public class ExtendedMemoryParamUnsetRequestData : INitroRequestData
+    {
+        [JsonPropertyName("memlimit")]public bool MemoryLimit => true;
+    }
+}
