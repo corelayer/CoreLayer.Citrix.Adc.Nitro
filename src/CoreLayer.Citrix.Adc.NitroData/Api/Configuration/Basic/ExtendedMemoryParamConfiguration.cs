9c332beecfed0b041c6567e3f84140e88f72f685
Niels Desopper <niels.desopper@corelayer.eu>
2020-08-14 10:07:22 +0200

    ExtendedMemoryParamGetCommand + configuration-object + tests

---
+++ src/CoreLayer.Citrix.Adc.NitroData/Api/Configuration/Basic/ExtendedMemoryParamConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace CoreLayer.Citrix.Adc.NitroData.Api.Configuration.Basic
+{
+    public class ExtendedMemoryParamConfiguration
+    {
+        [JsonPropertyName("memlimitactive")]public double? MemoryLimitActive { get; set; }
+        [JsonPropertyName("maxmemlimit")]public double? MaxMemoryLimit { get; set; }
+        [JsonPropertyName("memlimit")]public double? MemoryLimit { get; set; }
+        [JsonPropertyName("minrequiredmemory")]public double? MinRequiredMemory { get; set; }
+    }
+}
---
+++ src/CoreLayer.Citrix.Adc.NitroDataTests/Api/Configuration/Basic/ExtendedMemoryParam/ExtendedMemoryParamConfigurationTest.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text.Json;
+using CoreLayer.Citrix.Adc.NitroClient;
+using CoreLayer.Citrix.Adc.NitroData.Api.Configuration.Basic;
+using Xunit;
+
+namespace CoreLayer.Citrix.Adc.NitroDataTests.Api.Configuration.Basic.ExtendedMemoryParam
+{
+    public class ExtendedMemoryParamConfigurationTest
+    {
+        [Theory]
+        [ClassData(typeof(ExtendedMemoryParamConfigurationValidationTestData))]
+        public void ExtendedMemoryParamConfigurationValidationTest(ExtendedMemoryParamConfiguration configuration,
+            Dictionary<string, string> expected)
+        {
+            Assert.Equal(expected["ContentString"], JsonSerializer.Serialize(configuration, typeof(ExtendedMemoryParamConfiguration), NitroServiceSerializerOptions.JsonSerializerOptions));
+        }
+    }
+
+    public class ExtendedMemoryParamConfigurationValidationTestData : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            yield return new object[]
+            {
+                new ExtendedMemoryParamConfiguration()
+                {
+
+                },
+                new Dictionary<string, string>
+                {
+                    {"ContentString", "{}"}
+                }
+            };
+            yield return new object[]
+            {
+                new ExtendedMemoryParamConfiguration()
+                {
+                    MemoryLimit = 10,
+                    MemoryLimitActive = 100,
+                },
+                new Dictionary<string, string>
+                {
+                    {"ContentString", "{\"memlimitactive\":100,\"memlimit\":10}"}
+                }
+            };
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
