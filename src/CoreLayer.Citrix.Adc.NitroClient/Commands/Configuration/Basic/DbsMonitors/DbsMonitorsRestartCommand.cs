54d17a6d174f6f93f6864b88398bb08a9d407098
Niels Desopper <niels.desopper@corelayer.eu>
2020-08-14 09:18:44 +0200

    DbsMonitorsRestartCommand + test

---
+++ src/CoreLayer.Citrix.Adc.NitroClient/Commands/Configuration/Basic/DbsMonitors/DbsMonitorsRestartCommand.cs
@@ -0,0 +1,11 @@
+﻿using CoreLayer.Citrix.Adc.NitroClient.Api.Configuration.Basic.DbsMonitors;
+using CoreLayer.Citrix.Adc.NitroClient.Interfaces;
+
+namespace CoreLayer.Citrix.Adc.NitroClient.Commands.Configuration.Basic.DbsMonitors
+{
+    public class DbsMonitorsRestartCommand : NitroCommand<DbsMonitorsRestartResponse>
+    {
+        public DbsMonitorsRestartCommand(INitroServiceClient serviceClient, INitroRequestConfiguration requestConfiguration) 
+            :base(serviceClient, requestConfiguration){}
+    }
+}
---
+++ src/CoreLayer.Citrix.Adc.NitroClientTests/Api/Configuration/Basic/DbsMonitors/DbsMonitorsRestartRequestConfigurationTest.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using CoreLayer.Citrix.Adc.NitroClient.Api.Configuration.Basic.DbsMonitors;
+using Xunit;
+
+namespace CoreLayer.Citrix.Adc.NitroClientTests.Api.Configuration.Basic.DbsMonitors
+{
+    public class DbsMonitorsRestartRequestConfigurationTest
+    {
+        [Theory]
+        [ClassData(typeof(DbsMonitorsRestartRequestConfigurationValidationTestData))]
+        public void DbsMonitorsRestartRequestConfigurationValidationTest(
+            DbsMonitorsRestartRequestConfiguration configuration, Dictionary<string, string> expected)
+        {
+            Assert.Equal(expected["ResourcePath"], configuration.ResourcePath);
+            Assert.Equal(expected["RequestUri"], configuration.GenerateHttpRequestMessageAsync().Result.RequestUri.ToString());
+            Assert.Equal(expected["Method"], configuration.Method.ToString());
+            Assert.Equal(expected["Method"], configuration.GenerateHttpRequestMessageAsync().Result.Method.ToString());
+            Assert.Equal(expected["RequestContent"], configuration.GenerateHttpRequestMessageAsync().Result.Content.ReadAsStringAsync().Result);
+            Assert.Equal(expected["Options"], configuration.Options.ToString());
+        }
+    }
+
+    public class DbsMonitorsRestartRequestConfigurationValidationTestData : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            yield return new object[]
+            {
+                new DbsMonitorsRestartRequestConfiguration(), 
+                new Dictionary<string, string>
+                {
+                    {"Method", "POST"},
+                    {"ResourcePath", "/nitro/v1/config/dbsmonitors"},
+                    {"RequestUri", "/nitro/v1/config/dbsmonitors?action=restart"},
+                    {"RequestContent", "{\"dbsmonitors\":{}}"},
+                    {"Options", "?action=restart"}
+                }
+
+            };
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
