39973faf0c117ce3e571f05d26bdf5f54d366700
Niels Desopper <niels.desopper@corelayer.eu>
2020-09-04 11:56:52 +0200

    LocationDataClearCommand + tests

---
+++ src/CoreLayer.Citrix.Adc.NitroClient/Commands/Configuration/Basic/LocationData/LocationDataClearCommand.cs
@@ -0,0 +1,11 @@
+﻿using CoreLayer.Citrix.Adc.NitroClient.Api.Configuration.Basic.LocationData;
+using CoreLayer.Citrix.Adc.NitroClient.Interfaces;
+
+namespace CoreLayer.Citrix.Adc.NitroClient.Commands.Configuration.Basic.LocationData
+{
+    public class LocationDataClearCommand : NitroCommand<LocationDataClearResponse>
+    {
+        public LocationDataClearCommand(INitroServiceClient serviceClient, INitroRequestConfiguration requestConfiguration) 
+            :base(serviceClient, requestConfiguration){}
+    }
+}
---
+++ src/CoreLayer.Citrix.Adc.NitroClientTests/Api/Configuration/Basic/LocationData/LocationDataClearRequestConfigurationTest.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using CoreLayer.Citrix.Adc.NitroClient.Api.Configuration.Basic.LocationData;
+using Xunit;
+
+namespace CoreLayer.Citrix.Adc.NitroClientTests.Api.Configuration.Basic.LocationData
+{
+    public class LocationDataClearRequestConfigurationTest
+    {
+        [Theory]
+        [ClassData(typeof(LocationDataClearRequestConfigurationValidationTestData))]
+        public void LocationDataClearRequestConfigurationValidationTest(
+            LocationDataClearRequestConfiguration configuration, Dictionary<string, string> expected)
+        {
+            Assert.Equal(expected["ResourcePath"], configuration.ResourcePath);
+            Assert.Equal(expected["RequestUri"], configuration.GenerateHttpRequestMessageAsync().Result.RequestUri.ToString());
+            Assert.Equal(expected["Method"], configuration.Method.ToString());
+            Assert.Equal(expected["Method"], configuration.GenerateHttpRequestMessageAsync().Result.Method.ToString());
+            Assert.Equal(expected["Options"], configuration.Options.ToString());
+        }
+    }
+
+    public class LocationDataClearRequestConfigurationValidationTestData : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            yield return new object[]
+            {
+                new LocationDataClearRequestConfiguration(),
+                new Dictionary<string, string>
+                {
+                    {"Method", "POST"},
+                    {"ResourcePath", "/nitro/v1/config/locationdata"},
+                    {"RequestUri", "/nitro/v1/config/locationdata?action=clear"},
+                    {"Options", "?action=clear"},
+                },
+            };
+
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
