1f1904e03882ff5e2541f3beb84de026adae90c7
Niels Desopper <niels.desopper@corelayer.eu>
2020-09-07 10:56:27 +0200

    LocationFile6RemoveCommand + tests

---
+++ src/CoreLayer.Citrix.Adc.NitroClient/Commands/Configuration/Basic/LocationFile6/LocationFile6RemoveCommand.cs
@@ -0,0 +1,11 @@
+﻿using CoreLayer.Citrix.Adc.NitroClient.Api.Configuration.Basic.LocationFile6;
+using CoreLayer.Citrix.Adc.NitroClient.Interfaces;
+
+namespace CoreLayer.Citrix.Adc.NitroClient.Commands.Configuration.Basic.LocationFile6
+{
+    public class LocationFile6RemoveCommand : NitroCommand<LocationFile6RemoveResponse>
+    {
+        public LocationFile6RemoveCommand(INitroServiceClient serviceClient, INitroRequestConfiguration requestConfiguration) 
+            :base(serviceClient, requestConfiguration){}
+    }
+}
