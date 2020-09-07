4e46f94a01114fa96f8d0d5031ee05a2c2217e21
Niels Desopper <niels.desopper@corelayer.eu>
2020-09-07 11:47:32 +0200

    LocationFile6ImportCommand + tests

---
+++ src/CoreLayer.Citrix.Adc.NitroClient/Api/Configuration/Basic/LocationFile6/LocationFile6ImportRequestData.cs
@@ -0,0 +1,8 @@
+﻿namespace CoreLayer.Citrix.Adc.NitroClient.Api.Configuration.Basic.LocationFile6
+{
+    public class LocationFile6ImportRequestData
+    {
+        public string LocationFile { get; set; }
+        public string Src { get; set; }
+    }
+}
---
+++ src/CoreLayer.Citrix.Adc.NitroClient/Commands/Configuration/Basic/LocationFile6/LocationFile6ImportCommand.cs
@@ -0,0 +1,11 @@
+﻿using CoreLayer.Citrix.Adc.NitroClient.Api.Configuration.Basic.LocationFile6;
+using CoreLayer.Citrix.Adc.NitroClient.Interfaces;
+
+namespace CoreLayer.Citrix.Adc.NitroClient.Commands.Configuration.Basic.LocationFile6
+{
+    public class LocationFile6ImportCommand : NitroCommand<LocationFile6ImportResponse>
+    {
+        public LocationFile6ImportCommand(INitroServiceClient serviceClient, INitroRequestConfiguration requestConfiguration) 
+            :base(serviceClient, requestConfiguration){}
+    }
+}
