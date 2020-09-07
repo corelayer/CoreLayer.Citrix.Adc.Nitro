08fc73348613e408d7327f2fa997807373e653e7
Niels Desopper <niels.desopper@corelayer.eu>
2020-09-07 09:40:57 +0200

    LocationFileRemoveCommand + tests

---
+++ src/CoreLayer.Citrix.Adc.NitroClient/Api/Configuration/Basic/LocationFile/LocationFileRemoveRequestConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using System.Net.Http;
+using CoreLayer.Citrix.Adc.NitroClient.Interfaces;
+
+namespace CoreLayer.Citrix.Adc.NitroClient.Api.Configuration.Basic.LocationFile
+{
+    public class LocationFileRemoveRequestConfiguration : NitroRequestConfiguration
+    {
+        public override HttpMethod Method => HttpMethod.Delete;
+        public override string ResourcePath => "/nitro/v1/config/locationfile";
+        public override INitroRequestDataRoot DataRoot => new LocationFileRemoveRequestDataRoot();
+        public override INitroRequestOptions Options { get; set; } = new LocationFileRemoveRequestOptions();
+    }
+}
