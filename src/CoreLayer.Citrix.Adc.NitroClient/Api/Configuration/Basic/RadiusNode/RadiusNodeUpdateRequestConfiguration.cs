0a50aa17ed85aa692c66f3f7b829d30999b0a2bd
Niels Desopper <niels.desopper@corelayer.eu>
2020-09-10 10:43:13 +0200

    RadiusNodeUpdateCommand + tests

---
+++ src/CoreLayer.Citrix.Adc.NitroClient/Api/Configuration/Basic/RadiusNode/RadiusNodeUpdateRequestConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using System.Net.Http;
+using CoreLayer.Citrix.Adc.NitroClient.Interfaces;
+
+namespace CoreLayer.Citrix.Adc.NitroClient.Api.Configuration.Basic.RadiusNode
+{
+    public class RadiusNodeUpdateRequestConfiguration : NitroRequestConfiguration
+    {
+        public override HttpMethod Method => HttpMethod.Put;
+        public override string ResourcePath => "/nitro/v1/config/radiusnode";
+        public override INitroRequestDataRoot DataRoot { get; } 
+        public override INitroRequestOptions Options => new RadiusNodeUpdateRequestOptions();
+        
+        public RadiusNodeUpdateRequestConfiguration(RadiusNodeUpdateRequestDataRoot dataRoot)
+        {
+            DataRoot = dataRoot;
+        }
+    }
+}
---
+++ src/CoreLayer.Citrix.Adc.NitroClient/Commands/Configuration/Basic/RadiusNode/RadiusNodeUpdateCommand.cs
@@ -0,0 +1,11 @@
+﻿using CoreLayer.Citrix.Adc.NitroClient.Api.Configuration.Basic.RadiusNode;
+using CoreLayer.Citrix.Adc.NitroClient.Interfaces;
+
+namespace CoreLayer.Citrix.Adc.NitroClient.Commands.Configuration.Basic.RadiusNode
+{
+    public class RadiusNodeUpdateCommand : NitroCommand<RadiusNodeUpdateResponse>
+    {
+        public RadiusNodeUpdateCommand(INitroServiceClient serviceClient, INitroRequestConfiguration requestConfiguration) 
+            :base(serviceClient, requestConfiguration){}
+    }
+}
