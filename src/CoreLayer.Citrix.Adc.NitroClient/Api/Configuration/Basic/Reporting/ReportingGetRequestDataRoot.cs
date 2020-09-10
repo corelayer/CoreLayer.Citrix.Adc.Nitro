99873be48dc3273cb8a84999ad9d60b4711ca040
Niels Desopper <niels.desopper@corelayer.eu>
2020-09-10 11:51:24 +0200

    ReportingGetCommand + tests

---
+++ src/CoreLayer.Citrix.Adc.NitroClient/Api/Configuration/Basic/Reporting/ReportingGetRequestDataRoot.cs
@@ -0,0 +1,9 @@
+﻿using CoreLayer.Citrix.Adc.NitroClient.Interfaces;
+
+namespace CoreLayer.Citrix.Adc.NitroClient.Api.Configuration.Basic.Reporting
+{
+    public class ReportingGetRequestDataRoot : INitroRequestDataRoot
+    {
+        
+    }
+}
---
+++ src/CoreLayer.Citrix.Adc.NitroClient/Commands/Configuration/Basic/Reporting/ReportingGetCommand.cs
@@ -0,0 +1,11 @@
+﻿using CoreLayer.Citrix.Adc.NitroClient.Api.Configuration.Basic.Reporting;
+using CoreLayer.Citrix.Adc.NitroClient.Interfaces;
+
+namespace CoreLayer.Citrix.Adc.NitroClient.Commands.Configuration.Basic.Reporting
+{
+    public class ReportingGetCommand : NitroCommand<ReportingGetResponse>
+    {
+        public ReportingGetCommand(INitroServiceClient serviceClient, INitroRequestConfiguration requestConfiguration) 
+            :base(serviceClient, requestConfiguration){}
+    }
+}
