ea0cd3c3dce6abec555ce5952cca69f2d576e379
Niels Desopper <niels.desopper@corelayer.eu>
2020-09-10 11:04:28 +0200

    RadiusNodeRemoveCommand + tests

---
+++ src/CoreLayer.Citrix.Adc.NitroClient/Commands/Configuration/Basic/RadiusNode/RadiusNodeRemoveCommand.cs
@@ -0,0 +1,11 @@
+﻿using CoreLayer.Citrix.Adc.NitroClient.Api.Configuration.Basic.RadiusNode;
+using CoreLayer.Citrix.Adc.NitroClient.Interfaces;
+
+namespace CoreLayer.Citrix.Adc.NitroClient.Commands.Configuration.Basic.RadiusNode
+{
+    public class RadiusNodeRemoveCommand : NitroCommand<RadiusNodeRemoveResponse>
+    {
+        public RadiusNodeRemoveCommand(INitroServiceClient serviceClient, INitroRequestConfiguration requestConfiguration) 
+            :base(serviceClient, requestConfiguration){}
+    }
+}
