53225310407d1d18052ae761ab8609113049b90e
Niels Desopper <niels.desopper@corelayer.eu>
2020-09-10 10:19:31 +0200

    RadiusNodeAddCommand + tests

---
+++ src/CoreLayer.Citrix.Adc.NitroClient/Api/Configuration/Basic/RadiusNode/RadiusNodeAddRequestConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using System.Net.Http;
+using CoreLayer.Citrix.Adc.NitroClient.Interfaces;
+
+namespace CoreLayer.Citrix.Adc.NitroClient.Api.Configuration.Basic.RadiusNode
+{
+    public class RadiusNodeAddRequestConfiguration : NitroRequestConfiguration
+    {
+        public override HttpMethod Method => HttpMethod.Post;
+        public override string ResourcePath => "/nitro/v1/config/radiusnode";
+        public override INitroRequestDataRoot DataRoot { get; } 
+        public override INitroRequestOptions Options => new RadiusNodeAddRequestOptions();
+        
+        public RadiusNodeAddRequestConfiguration(RadiusNodeAddRequestDataRoot dataRoot)
+        {
+            DataRoot = dataRoot;
+        }
+    }
+}
---
+++ src/CoreLayer.Citrix.Adc.NitroClientTests/Api/Configuration/Basic/RadiusNode/RadiusNodeAddRequestConfigurationTest.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using CoreLayer.Citrix.Adc.NitroClient.Api.Configuration.Basic.RadiusNode;
+using Xunit;
+
+namespace CoreLayer.Citrix.Adc.NitroClientTests.Api.Configuration.Basic.RadiusNode
+{
+    public class RadiusNodeAddRequestConfigurationTest
+    {
+        [Theory]
+        [ClassData(typeof(RadiusNodeAddRequestConfigurationValidationTestData))]
+        public void RadiusNodeAddRequestConfigurationValidationTest(RadiusNodeAddRequestConfiguration configuration,
+            Dictionary<string, string> expected)
+        {
+            Assert.Equal(expected["ResourcePath"], configuration.ResourcePath);
+            Assert.Equal(expected["RequestUri"], configuration.GenerateHttpRequestMessageAsync().Result.RequestUri.ToString());
+            Assert.Equal(expected["Method"], configuration.Method.ToString());
+            Assert.Equal(expected["Method"], configuration.GenerateHttpRequestMessageAsync().Result.Method.ToString());
+            Assert.Equal(expected["RequestContent"], configuration.GenerateHttpRequestMessageAsync().Result.Content.ReadAsStringAsync().Result);
+            Assert.Equal(expected["Options"], configuration.Options.ToString());
+        }
+    }
+
+    public class RadiusNodeAddRequestConfigurationValidationTestData : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            yield return new object[]
+            {
+                new RadiusNodeAddRequestConfiguration(new RadiusNodeAddRequestDataRoot(new RadiusNodeAddRequestData()
+                {
+                    NodePrefix = "5",
+                    RadId = "1"
+                })),
+                new Dictionary<string, string>
+                {
+                    {"Method", "POST"},
+                    {"ResourcePath", "/nitro/v1/config/radiusnode"},
+                    {"RequestUri", "/nitro/v1/config/radiusnode"},
+                    {"RequestContent", "{\"radiusnode\":[{\"radid\":\"1\",\"nodeprefix\":\"5\"}]}"},
+                    {"Options", ""}
+                }
+            };
+            yield return new object[]
+            {
+                new RadiusNodeAddRequestConfiguration(
+                    new RadiusNodeAddRequestDataRoot(new dynamic[]
+                    {
+                        new RadiusNodeAddRequestData()
+                        {
+                            
+                        }
+                    } )
+                ),
+                new Dictionary<string, string>
+                {
+                    {"Method", "POST"},
+                    {"ResourcePath", "/nitro/v1/config/radiusnode"},
+                    {"RequestUri", "/nitro/v1/config/radiusnode"},
+                    {"RequestContent", "{\"radiusnode\":[{}]}"},
+                    {"Options", ""}
+                }
+            };
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
