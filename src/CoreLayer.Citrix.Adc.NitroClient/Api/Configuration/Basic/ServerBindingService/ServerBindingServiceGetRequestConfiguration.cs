24281bcb2766d4cb1ae424364dbc65fd98ebf413
Niels Desopper <niels.desopper@corelayer.eu>
2020-09-11 12:00:29 +0200

    ServerBindingServiceGetCommand + tests

---
+++ src/CoreLayer.Citrix.Adc.NitroClient/Api/Configuration/Basic/ServerBindingService/ServerBindingServiceGetRequestConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using System.Net.Http;
+using CoreLayer.Citrix.Adc.NitroClient.Interfaces;
+
+namespace CoreLayer.Citrix.Adc.NitroClient.Api.Configuration.Basic.ServerBindingService
+{
+    public class ServerBindingServiceGetRequestConfiguration : NitroRequestConfiguration
+    {
+        public override HttpMethod Method => HttpMethod.Get;
+        public override string ResourcePath => "/nitro/v1/config/server_service_binding";
+        public override INitroRequestOptions Options { get; set; } = new ServerBindingServiceGetRequestOptions();
+        public override INitroRequestDataRoot DataRoot => new ServerBindingServiceGetRequestDataRoot();
+    }
+}
---
+++ src/CoreLayer.Citrix.Adc.NitroClient/Commands/Configuration/Basic/ServerBindingService/ServerBindingServiceGetCommand.cs
@@ -0,0 +1,11 @@
+﻿using CoreLayer.Citrix.Adc.NitroClient.Api.Configuration.Basic.ServerBindingService;
+using CoreLayer.Citrix.Adc.NitroClient.Interfaces;
+
+namespace CoreLayer.Citrix.Adc.NitroClient.Commands.Configuration.Basic.ServerBindingService
+{
+    public class ServerBindingServiceGetCommand : NitroCommand<ServerBindingServiceGetResponse>
+    {
+        public ServerBindingServiceGetCommand(INitroServiceClient serviceClient, INitroRequestConfiguration requestConfiguration)
+            :base(serviceClient, requestConfiguration){}
+    }
+}
