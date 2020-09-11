15114989ce5e555183d60ec0c81521bd78809e5f
Niels Desopper <niels.desopper@corelayer.eu>
2020-09-11 08:58:32 +0200

    ReportingDisableCommand + tests

---
+++ src/CoreLayer.Citrix.Adc.NitroClient/Api/Configuration/Basic/Reporting/ReportingDisableRequestConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using System.Net.Http;
+using CoreLayer.Citrix.Adc.NitroClient.Interfaces;
+
+namespace CoreLayer.Citrix.Adc.NitroClient.Api.Configuration.Basic.Reporting
+{
+    public class ReportingDisableRequestConfiguration : NitroRequestConfiguration
+    {
+        public override HttpMethod Method => HttpMethod.Post;
+        public override string ResourcePath => "/nitro/v1/config/reporting";
+        public override INitroRequestDataRoot DataRoot => new ReportingDisableRequestDataRoot();
+        public override INitroRequestOptions Options => new ReportingDisableRequestOptions(){Action = "disable"};
+        
+        public ReportingDisableRequestConfiguration()
+        {
+            
+        }
+    }
+}
---
+++ src/CoreLayer.Citrix.Adc.NitroClientTests/Api/Configuration/Basic/Reporting/ReportingDisableRequestConfigurationTest.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using CoreLayer.Citrix.Adc.NitroClient.Api.Configuration.Basic.Reporting;
+using Xunit;
+
+namespace CoreLayer.Citrix.Adc.NitroClientTests.Api.Configuration.Basic.Reporting
+{
+    public class ReportingDisableRequestConfigurationTest
+    {
+        [Theory]
+        [ClassData(typeof(ReportingDisableRequestConfigurationValidationTestData))]
+        public void ReportingDisableRequestConfigurationValidationTest(
+            ReportingDisableRequestConfiguration configuration, Dictionary<string, string> expected)
+        {
+            Assert.Equal(expected["ResourcePath"], configuration.ResourcePath);
+            Assert.Equal(expected["RequestUri"], configuration.GenerateHttpRequestMessageAsync().Result.RequestUri.ToString());
+            Assert.Equal(expected["Method"], configuration.Method.ToString());
+            Assert.Equal(expected["Method"], configuration.GenerateHttpRequestMessageAsync().Result.Method.ToString());
+            Assert.Equal(expected["RequestContent"], configuration.GenerateHttpRequestMessageAsync().Result.Content.ReadAsStringAsync().Result);
+            Assert.Equal(expected["Options"], configuration.Options.ToString());
+        }
+    }
+
+    public class ReportingDisableRequestConfigurationValidationTestData : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            yield return new object[]
+            {
+                new ReportingDisableRequestConfiguration(), 
+                new Dictionary<string, string>
+                {
+                    {"Method", "POST"},
+                    {"ResourcePath", "/nitro/v1/config/reporting"},
+                    {"RequestUri", "/nitro/v1/config/reporting?action=disable"},
+                    {"RequestContent", "{\"reporting\":{}}"},
+                    {"Options", "?action=disable"}
+                }
+
+            };
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
