79cd6136a253f3b96ac7027929646793241e87f0
Niels Desopper <niels.desopper@corelayer.eu>
2020-09-15 10:06:31 +0200

    VServerRemoveCommand + test

---
+++ src/CoreLayer.Citrix.Adc.NitroClient/Api/Configuration/Basic/VServer/VServerRemoveRequestConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using System.Net.Http;
+using CoreLayer.Citrix.Adc.NitroClient.Interfaces;
+
+namespace CoreLayer.Citrix.Adc.NitroClient.Api.Configuration.Basic.VServer
+{
+    public class VServerRemoveRequestConfiguration : NitroRequestConfiguration
+    {
+        public override HttpMethod Method => HttpMethod.Delete;
+        public override string ResourcePath => "/nitro/v1/config/vserver";
+        public override INitroRequestDataRoot DataRoot => new VServerRemoveRequestDataRoot();
+        public override INitroRequestOptions Options { get; set; } = new VServerRemoveRequestOptions();
+    }
+}
