f293cdc2c50f2618991c43a2da4551eea2f315ba
Niels Desopper <niels.desopper@corelayer.eu>
2020-09-15 11:49:14 +0200

    CsactionUnsetCommand + test

---
+++ src/CoreLayer.Citrix.Adc.NitroClient/Api/Configuration/ContentSwitching/Csaction/CsactionUnsetRequestData.cs
@@ -0,0 +1,10 @@
+﻿using CoreLayer.Citrix.Adc.NitroClient.Interfaces;
+
+namespace CoreLayer.Citrix.Adc.NitroClient.Api.Configuration.ContentSwitching.Csaction
+{
+    public class CsactionUnsetRequestData : INitroRequestData
+    {
+        public string Name { get; set; }
+        public bool Comment = true;
+    }
+}
---
+++ src/CoreLayer.Citrix.Adc.NitroClient/Api/Configuration/ContentSwitching/Csaction/CsactionUnsetRequestDataRoot.cs
@@ -0,0 +1,14 @@
+﻿using CoreLayer.Citrix.Adc.NitroClient.Interfaces;
+
+namespace CoreLayer.Citrix.Adc.NitroClient.Api.Configuration.ContentSwitching.Csaction
+{
+    public class CsactionUnsetRequestDataRoot : INitroRequestDataRoot
+    {
+        public CsactionUnsetRequestDataRoot(dynamic data)
+        {
+            Csaction = data;
+        }
+        
+        public dynamic Csaction { get; set; }
+    }
+}
