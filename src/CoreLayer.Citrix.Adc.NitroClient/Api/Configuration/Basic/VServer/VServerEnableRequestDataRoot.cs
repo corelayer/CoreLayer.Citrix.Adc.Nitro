ac3384b0ab686ee53ddaecca982f55c9864a06e6
Niels Desopper <niels.desopper@corelayer.eu>
2020-09-15 10:53:41 +0200

    VServerEnableCommand + test

---
+++ src/CoreLayer.Citrix.Adc.NitroClient/Api/Configuration/Basic/VServer/VServerEnableRequestDataRoot.cs
@@ -0,0 +1,14 @@
+﻿using CoreLayer.Citrix.Adc.NitroClient.Interfaces;
+
+namespace CoreLayer.Citrix.Adc.NitroClient.Api.Configuration.Basic.VServer
+{
+    public class VServerEnableRequestDataRoot : INitroRequestDataRoot
+    {
+        public VServerEnableRequestDataRoot(dynamic data)
+        {
+            VServer = data;
+        }
+        
+        public dynamic VServer { get; set; }
+    }
+}
