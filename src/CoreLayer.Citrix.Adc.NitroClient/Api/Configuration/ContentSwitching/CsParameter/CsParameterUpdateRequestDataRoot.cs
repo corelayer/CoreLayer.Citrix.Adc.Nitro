a4fd3d1fc80f770d48edf72b0df9dc91ef8867f9
Niels Desopper <niels.desopper@corelayer.eu>
2020-09-16 09:57:35 +0200

    CsParameterUpdateCommand + test

---
+++ src/CoreLayer.Citrix.Adc.NitroClient/Api/Configuration/ContentSwitching/CsParameter/CsParameterUpdateRequestDataRoot.cs
@@ -0,0 +1,14 @@
+﻿using CoreLayer.Citrix.Adc.NitroClient.Interfaces;
+
+namespace CoreLayer.Citrix.Adc.NitroClient.Api.Configuration.ContentSwitching.CsParameter
+{
+    public class CsParameterUpdateRequestDataRoot : INitroRequestDataRoot
+    {
+        public CsParameterUpdateRequestDataRoot(dynamic data)
+        {
+            CsParameter = data;
+        }
+        
+        public dynamic CsParameter { get; set; }
+    }
+}
---
+++ src/CoreLayer.Citrix.Adc.NitroClient/Commands/Configuration/ContentSwitching/CsParameter/CsParameterUpdateCommand.cs
@@ -0,0 +1,14 @@
+﻿using CoreLayer.Citrix.Adc.NitroClient.Api.Configuration.ContentSwitching.CsParameter;
+using CoreLayer.Citrix.Adc.NitroClient.Interfaces;
+
+namespace CoreLayer.Citrix.Adc.NitroClient.Commands.Configuration.ContentSwitching.CsParameter
+{
+    public class CsParameterUpdateCommand : NitroCommand<CsParameterUpdateResponse>
+    {
+        public CsParameterUpdateCommand(INitroServiceClient serviceClient, INitroRequestConfiguration requestConfiguration) :
+            base(serviceClient, requestConfiguration)
+        {
+            
+        }
+    }
+}
