4491b973500809c6a6bbe08440c95e1b46ccc3df
Niels Desopper <niels.desopper@corelayer.eu>
2020-09-16 12:02:01 +0200

    CsPolicyLabelGetommand + test

---
+++ src/CoreLayer.Citrix.Adc.NitroClient/Api/Configuration/ContentSwitching/CsPolicyLabel/CsPolicyLabelGetResponse.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json.Serialization;
+using CoreLayer.Citrix.Adc.NitroData.Api.Configuration.ContentSwitching;
+
+namespace CoreLayer.Citrix.Adc.NitroClient.Api.Configuration.ContentSwitching.CsPolicyLabel
+{
+    public class CsPolicyLabelGetResponse : NitroResponse
+    {
+        [JsonPropertyName("cspolicylabel")]
+        public CsPolicyLabelConfiguration CsPolicyLabelConfiguration { get; set; }
+    }
+}
---
+++ src/CoreLayer.Citrix.Adc.NitroData/Api/Configuration/ContentSwitching/CsPolicyLabelConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json.Serialization;
+
+namespace CoreLayer.Citrix.Adc.NitroData.Api.Configuration.ContentSwitching
+{
+    public class CsPolicyLabelConfiguration 
+    {
+        public string LabelName { get; set; }
+        public string CsPolicyLabelType { get; set; }
+        [JsonPropertyName("numpol")]public string NumberOfPolicies { get; set; }
+        public string Hits { get; set; }
+        public string PolicyName { get; set; }
+        public string Priority { get; set; }
+        public string TargetVServer { get; set; }
+        public string GoToPriorityExpression { get; set; }
+        public string LabelType { get; set; }
+        [JsonPropertyName("invoke_labelname")]public string InvokeLabelName { get; set; }
+    }
+}
