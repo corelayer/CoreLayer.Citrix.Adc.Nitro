823c8c30d1d3cbeafcdd6bc76d82cd47151f48bc
Niels Desopper <niels.desopper@corelayer.eu>
2020-09-17 10:36:11 +0200

    CsPolicyLabelBindingCsPolicyRemoveCommand + test

---
+++ src/CoreLayer.Citrix.Adc.NitroClient/Api/Configuration/ContentSwitching/CsPolicyLabelBinding/CsPolicyLabelBindingCsPolicyRemoveRequestOptions.cs
@@ -0,0 +1,7 @@
+﻿namespace CoreLayer.Citrix.Adc.NitroClient.Api.Configuration.ContentSwitching.CsPolicyLabelBinding
+{
+    public class CsPolicyLabelBindingCsPolicyRemoveRequestOptions : NitroRequestOptions
+    {
+        
+    }
+}
