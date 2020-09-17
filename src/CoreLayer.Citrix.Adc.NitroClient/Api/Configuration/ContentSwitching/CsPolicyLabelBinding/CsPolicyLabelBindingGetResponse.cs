6d37070f7102860c0de60a1f20d313367a746e0a
Niels Desopper <niels.desopper@corelayer.eu>
2020-09-17 11:39:54 +0200

    CsPolicyLabelBindingGetCommand + test

---
+++ src/CoreLayer.Citrix.Adc.NitroClient/Api/Configuration/ContentSwitching/CsPolicyLabelBinding/CsPolicyLabelBindingGetResponse.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json.Serialization;
+using CoreLayer.Citrix.Adc.NitroData.Api.Configuration.ContentSwitching;
+
+namespace CoreLayer.Citrix.Adc.NitroClient.Api.Configuration.ContentSwitching.CsPolicyLabelBinding
+{
+    public class CsPolicyLabelBindingGetResponse : NitroResponse
+    {
+        [JsonPropertyName("cspolicylabel_binding")]
+        public CsPolicyLabelBindingConfiguration[] CsPolicyLabelBindingConfigurations { get; set; }
+    }
+}
---
+++ src/CoreLayer.Citrix.Adc.NitroData/Api/Configuration/ContentSwitching/CsPolicyLabelBindingConfiguration.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json.Serialization;
+
+namespace CoreLayer.Citrix.Adc.NitroData.Api.Configuration.ContentSwitching
+{
+    public class CsPolicyLabelBindingConfiguration
+    {
+        public string LabelName { get; set; }
+        [JsonPropertyName("cspolicylabel_cspolicy_binding")]
+        public CsPolicyLabelBindingCsPolicyConfiguration[] CsPolicyLabelBindingCsPolicyConfigurations { get; set; }
+    }
+}
