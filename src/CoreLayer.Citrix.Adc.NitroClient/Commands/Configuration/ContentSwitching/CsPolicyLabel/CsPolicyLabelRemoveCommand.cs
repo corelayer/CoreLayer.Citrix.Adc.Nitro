14bbfc6f1ff7c6df19797da4989e3f91c2f3ec17
Niels Desopper <niels.desopper@corelayer.eu>
2020-09-17 08:39:59 +0200

    CsPolicyLabelRemoveCommand + test

---
+++ src/CoreLayer.Citrix.Adc.NitroClient/Commands/Configuration/ContentSwitching/CsPolicyLabel/CsPolicyLabelRemoveCommand.cs
@@ -0,0 +1,14 @@
+﻿using CoreLayer.Citrix.Adc.NitroClient.Api.Configuration.ContentSwitching.CsPolicyLabel;
+using CoreLayer.Citrix.Adc.NitroClient.Interfaces;
+
+namespace CoreLayer.Citrix.Adc.NitroClient.Commands.Configuration.ContentSwitching.CsPolicyLabel
+{
+    public class CsPolicyLabelRemoveCommand : NitroCommand<CsPolicyLabelRemoveResponse>
+    {
+        public CsPolicyLabelRemoveCommand(INitroServiceClient serviceClient, INitroRequestConfiguration requestConfiguration) : base(
+            serviceClient, requestConfiguration)
+        {
+            
+        }
+    }
+}
