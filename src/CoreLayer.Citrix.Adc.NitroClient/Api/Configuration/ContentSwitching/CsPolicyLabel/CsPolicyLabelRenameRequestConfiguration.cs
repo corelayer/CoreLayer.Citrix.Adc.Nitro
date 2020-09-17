c31d02676b4916a1cec532afa8368acb357b7dea
Niels Desopper <niels.desopper@corelayer.eu>
2020-09-17 09:34:23 +0200

    CsPolicyLabelRenameCommand + test

---
+++ src/CoreLayer.Citrix.Adc.NitroClient/Api/Configuration/ContentSwitching/CsPolicyLabel/CsPolicyLabelRenameRequestConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using System.Net.Http;
+using CoreLayer.Citrix.Adc.NitroClient.Interfaces;
+
+namespace CoreLayer.Citrix.Adc.NitroClient.Api.Configuration.ContentSwitching.CsPolicyLabel
+{
+    public class CsPolicyLabelRenameRequestConfiguration : NitroRequestConfiguration
+    {
+        public override HttpMethod Method => HttpMethod.Post;
+        public override string ResourcePath => "/nitro/v1/config/cspolicylabel";
+        public override INitroRequestDataRoot DataRoot { get; }
+        public override INitroRequestOptions Options => new CsPolicyLabelRenameRequestOptions(){Action = "rename"};
+                
+        public CsPolicyLabelRenameRequestConfiguration(CsPolicyLabelRenameRequestDataRoot dataRoot)
+        {
+            DataRoot = dataRoot;
+        }
+    }
+}
---
+++ src/CoreLayer.Citrix.Adc.NitroClient/Commands/Configuration/ContentSwitching/CsPolicyLabel/CsPolicyLabelRenameCommand.cs
@@ -0,0 +1,14 @@
+﻿using CoreLayer.Citrix.Adc.NitroClient.Api.Configuration.ContentSwitching.CsPolicyLabel;
+using CoreLayer.Citrix.Adc.NitroClient.Interfaces;
+
+namespace CoreLayer.Citrix.Adc.NitroClient.Commands.Configuration.ContentSwitching.CsPolicyLabel
+{
+    public class CsPolicyLabelRenameCommand : NitroCommand<CsPolicyLabelRenameResponse>
+    {
+        public CsPolicyLabelRenameCommand(INitroServiceClient serviceClient, INitroRequestConfiguration requestConfiguration) :
+            base(serviceClient, requestConfiguration)
+        {
+            
+        }
+    }
+}
