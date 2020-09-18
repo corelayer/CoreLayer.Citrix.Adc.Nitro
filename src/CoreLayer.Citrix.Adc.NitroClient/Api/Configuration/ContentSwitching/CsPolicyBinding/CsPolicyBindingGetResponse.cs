8ed2593f8a6586fb9fdb1d0175c39cb4446e3abd
Niels Desopper <niels.desopper@corelayer.eu>
2020-09-18 09:59:08 +0200

    CsPolicyBindingGetCommand + tests

---
+++ src/CoreLayer.Citrix.Adc.NitroClient/Api/Configuration/ContentSwitching/CsPolicyBinding/CsPolicyBindingGetResponse.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json.Serialization;
+using CoreLayer.Citrix.Adc.NitroData.Api.Configuration.ContentSwitching;
+
+namespace CoreLayer.Citrix.Adc.NitroClient.Api.Configuration.ContentSwitching.CsPolicyBinding
+{
+    public class CsPolicyBindingGetResponse : NitroResponse
+    {
+        [JsonPropertyName("cspolicy_binding")]
+        public CsPolicyBindingConfiguration[] CsPolicyBindingConfigurations { get; set; }
+    }
+}
---
+++ src/CoreLayer.Citrix.Adc.NitroClient/Commands/Configuration/ContentSwitching/CsPolicyBinding/CsPolicyBindingGetCommand.cs
@@ -0,0 +1,14 @@
+﻿using CoreLayer.Citrix.Adc.NitroClient.Api.Configuration.ContentSwitching.CsPolicyBinding;
+using CoreLayer.Citrix.Adc.NitroClient.Interfaces;
+
+namespace CoreLayer.Citrix.Adc.NitroClient.Commands.Configuration.ContentSwitching.CsPolicyBinding
+{
+    public class CsPolicyBindingGetCommand : NitroCommand<CsPolicyBindingGetResponse>
+    {
+        public CsPolicyBindingGetCommand(INitroServiceClient serviceClient, INitroRequestConfiguration requestConfiguration) : base(
+            serviceClient, requestConfiguration)
+        {
+            
+        }
+    }
+}
