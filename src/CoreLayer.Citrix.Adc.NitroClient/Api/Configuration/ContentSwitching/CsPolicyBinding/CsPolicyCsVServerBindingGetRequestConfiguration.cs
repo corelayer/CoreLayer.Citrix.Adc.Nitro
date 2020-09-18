658bb0c0c40241595726829d577437c45a5b6b9e
Niels Desopper <niels.desopper@corelayer.eu>
2020-09-18 09:33:35 +0200

    CsPolicyCsVServerBindingGetCommand + tests

---
+++ src/CoreLayer.Citrix.Adc.NitroClient/Api/Configuration/ContentSwitching/CsPolicyBinding/CsPolicyCsVServerBindingGetRequestConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using System.Net.Http;
+using CoreLayer.Citrix.Adc.NitroClient.Interfaces;
+
+namespace CoreLayer.Citrix.Adc.NitroClient.Api.Configuration.ContentSwitching.CsPolicyBinding
+{
+    public class CsPolicyCsVServerBindingGetRequestConfiguration : NitroRequestConfiguration
+    {
+        public override HttpMethod Method => HttpMethod.Get;
+        public override string ResourcePath => "/nitro/v1/config/cspolicy_csvserver_binding";
+        public override INitroRequestDataRoot DataRoot => new CsPolicyCsVServerBindingGetRequestDataRoot();
+        public override INitroRequestOptions Options { get; set; } = new CsPolicyCsVServerBindingGetRequestOptions();
+    }
+}
