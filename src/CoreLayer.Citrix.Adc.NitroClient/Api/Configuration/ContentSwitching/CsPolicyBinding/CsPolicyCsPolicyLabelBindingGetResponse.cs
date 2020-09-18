d381d2d6b1dc2ecf632ae59bd59a152ee1c990d9
Niels Desopper <niels.desopper@corelayer.eu>
2020-09-18 08:51:42 +0200

    minor fix

--- src/CoreLayer.Citrix.Adc.NitroClient/Api/Configuration/ContentSwitching/CsPolicyBinding/CsPolicyCsPolicyLabelBindingGetResponse.cs
+++ src/CoreLayer.Citrix.Adc.NitroClient/Api/Configuration/ContentSwitching/CsPolicyBinding/CsPolicyCsPolicyLabelBindingGetResponse.cs
@@ -1,7 +1,11 @@
-﻿namespace CoreLayer.Citrix.Adc.NitroClient.Api.Configuration.ContentSwitching.CsPolicyBinding
+﻿using System.Text.Json.Serialization;
+using CoreLayer.Citrix.Adc.NitroData.Api.Configuration.ContentSwitching;
+
+namespace CoreLayer.Citrix.Adc.NitroClient.Api.Configuration.ContentSwitching.CsPolicyBinding
 {
     public class CsPolicyCsPolicyLabelBindingGetResponse : NitroResponse
     {
-        
+        [JsonPropertyName("cspolicy_cspolicylabel_binding")]
+        public CsPolicyCsPolicyLabelBindingConfiguration[] CsPolicyCsPolicyLabelBindingConfigurations { get; set; }
     }
 }
