d818081f1b2bac3be1e97da6bed9d093d9432828
Niels Desopper <niels.desopper@corelayer.eu>
2020-09-18 12:00:09 +0200

    CsvserverAnalyticsProfileBindingGetCommand + test

---
+++ src/CoreLayer.Citrix.Adc.NitroClientTests/Api/Configuration/ContentSwitching/CsvserverBinding/CsvserverAnalyticsProfileBinding/CsvAPBindingGetRequestConfigurationTest.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using CoreLayer.Citrix.Adc.NitroClient.Api.Configuration.ContentSwitching.CsvserverBinding.CsvserverAnalyticsProfileBinding;
+using Xunit;
+
+namespace CoreLayer.Citrix.Adc.NitroClientTests.Api.Configuration.ContentSwitching.CsvserverBinding.CsvserverAnalyticsProfileBinding
+{
+    public class CsvserverAnalyticsProfileBindingGetRequestConfigurationTest
+    {
+        [Theory]
+        [ClassData(typeof(CsvserverAnalyticsProfileBindingGetRequestConfigurationValidationTestData))]
+        public void CsvserverAnalyticsProfileBindingGetRequestConfigurationValidationTest(
+            CsvserverAnalyticsProfileBindingGetRequestConfiguration configuration, Dictionary<string, string> expected)
+        {
+            Assert.Equal(expected["ResourcePath"], configuration.ResourcePath);
+            Assert.Equal(expected["RequestUri"], configuration.GenerateHttpRequestMessageAsync().Result.RequestUri.ToString());
+            Assert.Equal(expected["Method"], configuration.Method.ToString());
+            Assert.Equal(expected["Method"], configuration.GenerateHttpRequestMessageAsync().Result.Method.ToString());
+            Assert.Equal(expected["Options"], configuration.Options.ToString());
+        }
+    }
+
+    public class CsvserverAnalyticsProfileBindingGetRequestConfigurationValidationTestData : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            yield return new object[]
+            {
+                new CsvserverAnalyticsProfileBindingGetRequestConfiguration(),
+                new Dictionary<string, string>
+                {
+                    {"ResourcePath", "/nitro/v1/config/csvserver_analyticsprofile_binding"},
+                    {"RequestUri", "/nitro/v1/config/csvserver_analyticsprofile_binding?bulkbindings=yes"},
+                    {"Method", "GET"},
+                    {"Options", "?bulkbindings=yes"}
+                }
+            };
+            
+            yield return new object[]
+            {
+                new CsvserverAnalyticsProfileBindingGetRequestConfiguration
+                {
+                    Options = new CsvserverAnalyticsProfileBindingGetRequestOptions()
+                    {
+                        ResourceFilter = new Dictionary<string, string>(),
+                        PropertyFilter = new List<string>
+                        {
+                            "Filter"
+                        },
+                        ResourceName = "instance1"
+                    }
+                }, 
+                new Dictionary<string, string>
+                {
+                    {"ResourcePath", "/nitro/v1/config/csvserver_analyticsprofile_binding"},
+                    {"RequestUri", "/nitro/v1/config/csvserver_analyticsprofile_binding/instance1?attrs=Filter"},
+                    {"Method", "GET"},
+                    {"Options", "/instance1?attrs=Filter"},
+                }
+            };
+            yield return new object[]
+            {
+                new CsvserverAnalyticsProfileBindingGetRequestConfiguration
+                {
+                    Options = new CsvserverAnalyticsProfileBindingGetRequestOptions
+                    {
+                        PropertyFilter = new List<string>
+                        {
+                            "Name"
+                        },
+                        ResourceName = "instance1",
+                        Action = "testAction"
+                    }
+                }, 
+                new Dictionary<string, string>
+                {
+                    {"ResourcePath", "/nitro/v1/config/csvserver_analyticsprofile_binding"},
+                    {"RequestUri", "/nitro/v1/config/csvserver_analyticsprofile_binding/instance1?action=testAction"},
+                    {"Method", "GET"},
+                    {"Options", "/instance1?action=testAction"}
+                }
+            };
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ src/CoreLayer.Citrix.Adc.NitroDataTests/Api/Configuration/ContentSwitching/CsvserverBinding/CsvserverAnalyticsProfileBinding/CsvserverAnalyticsProfileBindingConfigurationTest.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.Collections;
+using CoreLayer.Citrix.Adc.NitroData.Api.Configuration.ContentSwitching.CsvserverBinding;
+using CoreLayer.Citrix.Adc.NitroClient;
+using System.Text.Json;
+using Xunit;
+
+namespace CoreLayer.Citrix.Adc.NitroDataTests.Api.Configuration.ContentSwitching.CsvserverBinding.CsvserverAnalyticsProfileBinding
+{
+    public class CsvserverAnalyticsProfileBindingConfigurationTest
+    {
+        [Theory]
+        [ClassData(typeof(CsvserverAnalyticsProfileBindingConfigurationValidationTestData))]
+        public void CsvserverAnalyticsProfileBindingConfigurationValidationTestData(
+            CsvserverAnalyticsProfileBindingConfiguration configuration, Dictionary<string, string> expected)
+        {
+            Assert.Equal(expected["ContentString"], JsonSerializer.Serialize(configuration, typeof(CsvserverAnalyticsProfileBindingConfiguration), NitroServiceSerializerOptions.JsonSerializerOptions));
+        }
+    }
+
+    public class CsvserverAnalyticsProfileBindingConfigurationValidationTestData : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            yield return new object[]
+            {
+                new CsvserverAnalyticsProfileBindingConfiguration()
+                {
+
+                },
+                new Dictionary<string, string>
+                {
+                    {"ContentString", "{}"}
+                }
+            };
+            yield return new object[]
+            {
+                new CsvserverAnalyticsProfileBindingConfiguration()
+                {
+                    Name = "test",
+                },
+                new Dictionary<string, string>
+                {
+                    {"ContentString", "{\"name\":\"test\"}"}
+                }
+            };
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
