3e513d41501e864df558f45f3b6d5968ba3a4b36
Niels Desopper <niels.desopper@corelayer.eu>
2020-09-22 11:48:47 +0200

    CsvserverAppFwPolicyBindingRemoveCommand + test

---
+++ src/CoreLayer.Citrix.Adc.NitroClient/Api/Configuration/ContentSwitching/CsvserverBinding/CsvserverAppFwPolicyBinding/CsvserverAppFwPolicyBindingRemoveRequestOptions.cs
@@ -0,0 +1,7 @@
+﻿namespace CoreLayer.Citrix.Adc.NitroClient.Api.Configuration.ContentSwitching.CsvserverBinding.CsvserverAppFwPolicyBinding
+{
+    public class CsvserverAppFwPolicyBindingRemoveRequestOptions : NitroRequestRemoveOptions
+    {
+        
+    }
+}
---
+++ src/CoreLayer.Citrix.Adc.NitroClientTests/Api/Configuration/ContentSwitching/CsvserverBinding/CsvserverAppFwPolicyBinding/CsvserverAFwPBindingRemoveRequestConfigurationTest.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Collections;
+using CoreLayer.Citrix.Adc.NitroClient.Api.Configuration.ContentSwitching.CsvserverBinding.CsvserverAppFwPolicyBinding;
+using Xunit;
+
+namespace CoreLayer.Citrix.Adc.NitroClientTests.Api.Configuration.ContentSwitching.CsvserverBinding.CsvserverAppFwPolicyBinding
+{
+    public class CsvserverAFwPBindingRemoveRequestConfigurationTest 
+    {
+        [Theory]
+        [ClassData(typeof(CsvserverAFwPBindingRemoveRequestConfigurationValidationTestData))]
+        public void CsvserverAFwPBindingRemoveRequestConfigurationValidationTest(
+            CsvserverAppFwPolicyBindingRemoveRequestConfiguration configuration, Dictionary<string, string> expected)
+        {
+            Assert.Equal(expected["ResourcePath"], configuration.ResourcePath);
+            Assert.Equal(expected["RequestUri"], configuration.GenerateHttpRequestMessageAsync().Result.RequestUri.ToString());
+            Assert.Equal(expected["Method"], configuration.Method.ToString());
+            Assert.Equal(expected["Method"], configuration.GenerateHttpRequestMessageAsync().Result.Method.ToString());
+            Assert.Equal(expected["Options"], configuration.Options.ToString());
+        }
+    }
+
+    public class CsvserverAFwPBindingRemoveRequestConfigurationValidationTestData : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            yield return new object[]
+            {
+                new CsvserverAppFwPolicyBindingRemoveRequestConfiguration()
+                {
+                    Options = new CsvserverAppFwPolicyBindingRemoveRequestOptions()
+                    {
+                        Arguments = new Dictionary<string, string>(){{"name", "testCsv"},{"policyname", "testPolicy"}}
+                    }
+                },
+                new Dictionary<string, string>()
+                {
+                    {"Method", "DELETE"},
+                    {"ResourcePath", "/nitro/v1/config/csvserver_appfwpolicy_binding"},
+                    {"RequestUri", "/nitro/v1/config/csvserver_appfwpolicy_binding?args=name:testCsv,policyname:testPolicy"},
+                    {"Options", "?args=name:testCsv,policyname:testPolicy"},
+                } 
+            };        
+        }        
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
