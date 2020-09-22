b3cbe73ed2f1800b4b29224c9c9a77a106ba63d0
Niels Desopper <niels.desopper@corelayer.eu>
2020-09-22 11:26:41 +0200

    CsvserverAppFwPolicyBindingAddCommand + test

---
+++ src/CoreLayer.Citrix.Adc.NitroClient/Api/Configuration/ContentSwitching/CsvserverBinding/CsvserverAppFwPolicyBinding/CsvserverAppFwPolicyBindingAddRequestDataRoot.cs
@@ -0,0 +1,20 @@
+﻿using CoreLayer.Citrix.Adc.NitroClient.Interfaces;
+
+namespace CoreLayer.Citrix.Adc.NitroClient.Api.Configuration.ContentSwitching.CsvserverBinding.CsvserverAppFwPolicyBinding
+{
+    public class CsvserverAppFwPolicyBindingAddRequestDataRoot : INitroRequestDataRoot
+    {
+        public CsvserverAppFwPolicyBindingAddRequestDataRoot(dynamic data)
+        {
+            Csvserver_AppFwPolicy_Binding = new dynamic[]{data};
+        }
+
+        public CsvserverAppFwPolicyBindingAddRequestDataRoot(dynamic[] data)
+        {
+            Csvserver_AppFwPolicy_Binding = data;
+        }
+        
+        // name doesnt follow convention because it gets serialized
+        public dynamic[] Csvserver_AppFwPolicy_Binding { get; set; }
+    }
+}
