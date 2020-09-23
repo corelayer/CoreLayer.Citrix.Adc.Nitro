c1e2913dc5dbc53a5630ce23180da60693e62262
Niels Desopper <niels.desopper@corelayer.eu>
2020-09-23 10:20:44 +0200

    CsvserverAppQoePolicyBindingRemoveCommand + test

---
+++ src/CoreLayer.Citrix.Adc.NitroClient/Api/Configuration/ContentSwitching/CsvserverBinding/CsvserverAppQoePolicyBinding/CsvserverAppQoePolicyBindingRemoveRequestConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using System.Net.Http;
+using CoreLayer.Citrix.Adc.NitroClient.Interfaces;
+
+namespace CoreLayer.Citrix.Adc.NitroClient.Api.Configuration.ContentSwitching.CsvserverBinding.CsvserverAppQoePolicyBinding
+{
+    public class CsvserverAppQoePolicyBindingRemoveRequestConfiguration : NitroRequestConfiguration
+    {
+        public override HttpMethod Method => HttpMethod.Delete;
+        public override string ResourcePath => "/nitro/v1/config/csvserver_appqoepolicy_binding";
+        public override INitroRequestDataRoot DataRoot => new CsvserverAppQoePolicyBindingRemoveRequestDataRoot();
+        public override INitroRequestOptions Options { get; set; } = new CsvserverAppQoePolicyBindingRemoveRequestOptions();
+    }
+}
