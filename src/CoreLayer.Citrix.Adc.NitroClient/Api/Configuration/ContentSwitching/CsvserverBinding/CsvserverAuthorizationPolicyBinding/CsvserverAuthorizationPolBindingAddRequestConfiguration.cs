56d4cc70d0ce670d78113a462e29307c32adb831
Niels Desopper <niels.desopper@corelayer.eu>
2020-09-24 10:59:46 +0200

    CsvserverAuthorizationPolicyBindingAddCommand + test

---
+++ src/CoreLayer.Citrix.Adc.NitroClient/Api/Configuration/ContentSwitching/CsvserverBinding/CsvserverAuthorizationPolicyBinding/CsvserverAuthorizationPolBindingAddRequestConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using System.Net.Http;
+using CoreLayer.Citrix.Adc.NitroClient.Interfaces;
+
+namespace CoreLayer.Citrix.Adc.NitroClient.Api.Configuration.ContentSwitching.CsvserverBinding.CsvserverAuthorizationPolicyBinding
+{
+    public class CsvserverAuthorizationPolicyBindingAddRequestConfiguration : NitroRequestConfiguration
+    {
+        public override HttpMethod Method => HttpMethod.Put;
+        public override string ResourcePath => "/nitro/v1/config/csvserver_authorizationpolicy_binding";
+        public override INitroRequestOptions Options => new CsvserverAuthorizationPolicyBindingAddRequestOptions();
+        public override INitroRequestDataRoot DataRoot { get; }
+
+        public CsvserverAuthorizationPolicyBindingAddRequestConfiguration(CsvserverAuthorizationPolicyBindingAddRequestDataRoot dataRoot)
+        {
+            DataRoot = dataRoot;
+        }
+    }
+}
---
+++ src/CoreLayer.Citrix.Adc.NitroClient/Api/Configuration/ContentSwitching/CsvserverBinding/CsvserverAuthorizationPolicyBinding/CsvserverAuthorizationPolicyBindingAddRequestDataRoot.cs
@@ -0,0 +1,20 @@
+﻿using CoreLayer.Citrix.Adc.NitroClient.Interfaces;
+
+namespace CoreLayer.Citrix.Adc.NitroClient.Api.Configuration.ContentSwitching.CsvserverBinding.CsvserverAuthorizationPolicyBinding
+{
+    public class CsvserverAuthorizationPolicyBindingAddRequestDataRoot : INitroRequestDataRoot
+    {
+        public CsvserverAuthorizationPolicyBindingAddRequestDataRoot(dynamic data)
+        {
+            Csvserver_AuthorizationPolicy_Binding = new dynamic[]{data};
+        }
+
+        public CsvserverAuthorizationPolicyBindingAddRequestDataRoot(dynamic[] data)
+        {
+            Csvserver_AuthorizationPolicy_Binding = data;
+        }
+        
+        // name doesnt follow convention because it gets serialized
+        public dynamic[] Csvserver_AuthorizationPolicy_Binding { get; set; }
+    }
+}
