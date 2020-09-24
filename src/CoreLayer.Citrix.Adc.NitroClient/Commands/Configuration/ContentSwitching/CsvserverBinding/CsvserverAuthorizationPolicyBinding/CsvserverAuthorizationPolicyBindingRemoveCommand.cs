2d894b1afca7fabf2455d9b9cf3a2b3ac1c15c31
Niels Desopper <niels.desopper@corelayer.eu>
2020-09-24 11:45:41 +0200

    CsvserverAuthorizationPolicyBindingRemoveCommand + test

---
+++ src/CoreLayer.Citrix.Adc.NitroClient/Commands/Configuration/ContentSwitching/CsvserverBinding/CsvserverAuthorizationPolicyBinding/CsvserverAuthorizationPolicyBindingRemoveCommand.cs
@@ -0,0 +1,11 @@
+﻿using CoreLayer.Citrix.Adc.NitroClient.Api.Configuration.ContentSwitching.CsvserverBinding.CsvserverAuthorizationPolicyBinding;
+using CoreLayer.Citrix.Adc.NitroClient.Interfaces;
+
+namespace CoreLayer.Citrix.Adc.NitroClient.Commands.Configuration.ContentSwitching.CsvserverBinding.CsvserverAuthorizationPolicyBinding
+{
+    public class CsvserverAuthorizationPolicyBindingRemoveCommand : NitroCommand<CsvserverAuthorizationPolicyBindingRemoveResponse>
+    {
+        public CsvserverAuthorizationPolicyBindingRemoveCommand(INitroServiceClient serviceClient, INitroRequestConfiguration requestConfiguration)
+            : base(serviceClient, requestConfiguration) { }
+    }
+}
---
+++ src/CoreLayer.Citrix.Adc.NitroClientTests/Api/Configuration/ContentSwitching/CsvserverBinding/CsvserverAuthorizationPolicyBinding/CsvserverAPBindingRemoveRequestConfigurationTest.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Collections;
+using CoreLayer.Citrix.Adc.NitroClient.Api.Configuration.ContentSwitching.CsvserverBinding.CsvserverAuthorizationPolicyBinding;
+using Xunit;
+
+namespace CoreLayer.Citrix.Adc.NitroClientTests.Api.Configuration.ContentSwitching.CsvserverBinding.CsvserverAuthorizationPolicyBinding
+{
+    public class CsvserverAuthorizationPolicyBindingRemoveRequestConfigurationTest
+    {
+        [Theory]
+        [ClassData(typeof(CsvserverAuthorizationPolicyBindingRemoveRequestConfigurationValidationTestData))]
+        public void CsvserverAuthorizationPolicyBindingRemoveRequestConfigurationValidationTest(
+            CsvserverAuthorizationPolicyBindingRemoveRequestConfiguration configuration,
+            Dictionary<string, string> expected)
+        {
+            Assert.Equal(expected["ResourcePath"], configuration.ResourcePath);
+            Assert.Equal(expected["RequestUri"], configuration.GenerateHttpRequestMessageAsync().Result.RequestUri.ToString());
+            Assert.Equal(expected["Method"], configuration.Method.ToString());
+            Assert.Equal(expected["Method"], configuration.GenerateHttpRequestMessageAsync().Result.Method.ToString());
+            Assert.Equal(expected["Options"], configuration.Options.ToString());
+        }
+    }
+
+    public class CsvserverAuthorizationPolicyBindingRemoveRequestConfigurationValidationTestData : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            yield return new object[]
+            {
+                new CsvserverAuthorizationPolicyBindingRemoveRequestConfiguration()
+                {
+                    Options = new CsvserverAuthorizationPolicyBindingRemoveRequestOptions()
+                    {
+                        Arguments = new Dictionary<string, string>(){{"name", "testCsv"},{"policyname", "testPolicy"}}
+                    }
+                },
+                new Dictionary<string, string>()
+                {
+                    {"Method", "DELETE"},
+                    {"ResourcePath", "/nitro/v1/config/csvserver_authorizationpolicy_binding"},
+                    {"RequestUri", "/nitro/v1/config/csvserver_authorizationpolicy_binding?args=name:testCsv,policyname:testPolicy"},
+                    {"Options", "?args=name:testCsv,policyname:testPolicy"},
+                } 
+            };        
+        }        
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
