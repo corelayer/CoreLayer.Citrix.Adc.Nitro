58950ae2f034c2c98ecd6af6e90b7983751a8cd7
Niels Desopper <niels.desopper@corelayer.eu>
2020-09-24 09:17:04 +0200

    CsvserverAuditSysLogPolicyBindingGetCommand + test

---
+++ src/CoreLayer.Citrix.Adc.NitroClientTests/Api/Configuration/ContentSwitching/CsvserverBinding/CsvserverAuditSysLogPolicyBinding/CsvserverASLPBindingGetRequestConfigurationTest.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections.Generic;
+using System.Collections;
+using CoreLayer.Citrix.Adc.NitroClient.Api.Configuration.ContentSwitching.CsvserverBinding.CsvserverAuditSysLogPolicyBinding;
+using Xunit;
+
+namespace CoreLayer.Citrix.Adc.NitroClientTests.Api.Configuration.ContentSwitching.CsvserverBinding.CsvserverAuditSysLogPolicyBinding
+{
+    public class CsvserverASLPBindingGetRequestConfigurationTest
+    {
+        [Theory]
+        [ClassData(typeof(CsvserverASLPBindingGetRequestConfigurationValidationTestData))]
+        public void CsvserverASLPBindingGetRequestConfigurationValidationTest(
+            CsvserverAuditSysLogPolicyBindingGetRequestConfiguration configuration, Dictionary<string, string> expected)
+        {
+            Assert.Equal(expected["ResourcePath"], configuration.ResourcePath);
+            Assert.Equal(expected["RequestUri"], configuration.GenerateHttpRequestMessageAsync().Result.RequestUri.ToString());
+            Assert.Equal(expected["Method"], configuration.Method.ToString());
+            Assert.Equal(expected["Method"], configuration.GenerateHttpRequestMessageAsync().Result.Method.ToString());
+            Assert.Equal(expected["Options"], configuration.Options.ToString());
+        }
+    }
+
+    public class CsvserverASLPBindingGetRequestConfigurationValidationTestData : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            yield return new object[]
+            {
+                new CsvserverAuditSysLogPolicyBindingGetRequestConfiguration(),
+                new Dictionary<string, string>
+                {
+                    {"ResourcePath", "/nitro/v1/config/csvserver_auditsyslogpolicy_binding"},
+                    {"RequestUri", "/nitro/v1/config/csvserver_auditsyslogpolicy_binding?bulkbindings=yes"},
+                    {"Method", "GET"},
+                    {"Options", "?bulkbindings=yes"}
+                }
+            };
+            
+            yield return new object[]
+            {
+                new CsvserverAuditSysLogPolicyBindingGetRequestConfiguration
+                {
+                    Options = new CsvserverAuditSysLogPolicyBindingGetRequestOptions()
+                    {
+                        ResourceFilter = new Dictionary<string, string>(),
+                        PropertyFilter = new List<string>
+                        {
+                            "Filter"
+                        },
+                        ResourceName = "instance1"
+                    }
+                }, 
+                new Dictionary<string, string>
+                {
+                    {"ResourcePath", "/nitro/v1/config/csvserver_auditsyslogpolicy_binding"},
+                    {"RequestUri", "/nitro/v1/config/csvserver_auditsyslogpolicy_binding/instance1?attrs=Filter"},
+                    {"Method", "GET"},
+                    {"Options", "/instance1?attrs=Filter"},
+                }
+            };
+            yield return new object[]
+            {
+                new CsvserverAuditSysLogPolicyBindingGetRequestConfiguration
+                {
+                    Options = new CsvserverAuditSysLogPolicyBindingGetRequestOptions
+                    {
+                        PropertyFilter = new List<string>
+                        {
+                            "Name"
+                        },
+                        ResourceName = "instance1",
+                        Action = "testAction"
+                    }
+                }, 
+                new Dictionary<string, string>
+                {
+                    {"ResourcePath", "/nitro/v1/config/csvserver_auditsyslogpolicy_binding"},
+                    {"RequestUri", "/nitro/v1/config/csvserver_auditsyslogpolicy_binding/instance1?action=testAction"},
+                    {"Method", "GET"},
+                    {"Options", "/instance1?action=testAction"}
+                }
+            };
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ src/CoreLayer.Citrix.Adc.NitroDataTests/Api/Configuration/ContentSwitching/CsvserverBinding/CsvserverAuditSysLogPolicyBinding/CsvserverAuditSysLogPolicyBindingConfigurationTest.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.Collections;
+using CoreLayer.Citrix.Adc.NitroData.Api.Configuration.ContentSwitching.CsvserverBinding;
+using Xunit;
+using CoreLayer.Citrix.Adc.NitroClient;
+using System.Text.Json;
+
+namespace CoreLayer.Citrix.Adc.NitroDataTests.Api.Configuration.ContentSwitching.CsvserverBinding.CsvserverAuditSysLogPolicyBinding
+{
+    public class CsvserverAuditSysLogPolicyBindingConfigurationTest
+    {
+        [Theory]
+        [ClassData(typeof(CsvserverAuditSysLogPolicyBindingConfigurationValidationTestData))]
+        public void CsvserverAuditSysLogPolicyBindingConfigurationValidationTest(
+            CsvserverAuditSysLogPolicyBindingConfiguration configuration, Dictionary<string, string> expected)
+        {
+            Assert.Equal(expected["ContentString"], JsonSerializer.Serialize(configuration, typeof(CsvserverAuditSysLogPolicyBindingConfiguration), NitroServiceSerializerOptions.JsonSerializerOptions));
+        }
+    }
+
+    public class CsvserverAuditSysLogPolicyBindingConfigurationValidationTestData : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            yield return new object[]
+            {
+                new CsvserverAuditSysLogPolicyBindingConfiguration()
+                {
+
+                },
+                new Dictionary<string, string>
+                {
+                    {"ContentString", "{}"}
+                }
+            };
+            yield return new object[]
+            {
+                new CsvserverAuditSysLogPolicyBindingConfiguration()
+                {
+                    Name = "test",
+                },
+                new Dictionary<string, string>
+                {
+                    {"ContentString", "{\"name\":\"test\"}"}
+                }
+            };
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
