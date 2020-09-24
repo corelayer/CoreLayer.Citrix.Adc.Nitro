27a87bd8cb363770980502f785a9125e26111165
Niels Desopper <niels.desopper@corelayer.eu>
2020-09-24 09:20:34 +0200

    minor fix

--- src/CoreLayer.Citrix.Adc.NitroClient/Api/Configuration/ContentSwitching/CsvserverBinding/CsvserverAuditSysLogPolicyBinding/CsvserverAuditSysLogPolicyBindingGetResponse.cs
+++ src/CoreLayer.Citrix.Adc.NitroClient/Api/Configuration/ContentSwitching/CsvserverBinding/CsvserverAuditSysLogPolicyBinding/CsvserverAuditSysLogPolicyBindingGetResponse.cs
@@ -1,7 +1,11 @@
-﻿namespace CoreLayer.Citrix.Adc.NitroClient.Api.Configuration.ContentSwitching.CsvserverBinding.CsvserverAuditSysLogPolicyBinding
+﻿using System.Text.Json.Serialization;
+using CoreLayer.Citrix.Adc.NitroData.Api.Configuration.ContentSwitching.CsvserverBinding;
+
+namespace CoreLayer.Citrix.Adc.NitroClient.Api.Configuration.ContentSwitching.CsvserverBinding.CsvserverAuditSysLogPolicyBinding
 {
     public class CsvserverAuditSysLogPolicyBindingGetResponse : NitroResponse
     {
-        
+        [JsonPropertyName("csvserver_auditsyslogpolicy_binding")]
+        public CsvserverAuditSysLogPolicyBindingConfiguration[] CsvserverAuditSysLogPolicyBindingConfigurations { get; set; }
     }
 }
