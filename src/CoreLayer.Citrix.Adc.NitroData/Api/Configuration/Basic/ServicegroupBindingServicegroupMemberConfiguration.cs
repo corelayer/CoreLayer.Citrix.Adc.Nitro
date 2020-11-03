ccbf4fd39e8c0a5cefb5df8b7a4bb72c6760e64a
Jan Tytgat <jan.tytgat@corelayer.eu>
2020-11-03 17:26:35 +0100

    Field types on ServicegroupBindingServicegroupMemberConfiguration

--- src/CoreLayer.Citrix.Adc.NitroData/Api/Configuration/Basic/ServicegroupBindingServicegroupMemberConfiguration.cs
+++ src/CoreLayer.Citrix.Adc.NitroData/Api/Configuration/Basic/ServicegroupBindingServicegroupMemberConfiguration.cs
@@ -11,16 +11,16 @@ namespace CoreLayer.Citrix.Adc.NitroData.Api.Configuration.Basic
         public string NameServer { get; set; }
         public string State { get; set; }
         public double? HashId { get; set; }
-        public double? ServerId { get; set; }
+        public string ServerId { get; set; }
         public string ServerName { get; set; }
         public string CustomServerId { get; set; }
-        public double? Weight { get; set; }
+        public string Weight { get; set; }
         [JsonPropertyName("dbsttl")]public double? DomainBasedServiceTtl { get; set; }
         [JsonPropertyName("svcitmpriority")]public double? ServiceItemPriority { get; set; }
         public double? Delay { get; set; }
         public string StateChangeTimeSec { get; set; }
         [JsonPropertyName("svrstate")]public string ServerState { get; set; }
-        public double? TicksSinceLastStateChange { get; set; }
+        public string TicksSinceLastStateChange { get; set; }
         public string TrofsReason { get; set; }
         public string Graceful { get; set; }
     }
